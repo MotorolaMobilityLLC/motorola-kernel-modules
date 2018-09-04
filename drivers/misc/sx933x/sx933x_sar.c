09942b267ebd4c41aef9b5ec337024682b495dd7
liugy11 <liugy11@lenovo.com>
2018-09-04 13:04:56 +0800

    capsensor sx9331: add suspend irq shielding
     suspend:enter sleep mode
     resume:exit sleep mode
    
    Change-Id: I24f762b45b5b792c659b167fd311e957258a8a03
    Signed-off-by: liugy11 <liugy11@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1237354
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jiyue Cui <cuijy2@lenovo.com>
    Reviewed-by: Ryan Lattrel <ryanl@motorola.com>
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 00606bae11dd496d947130a26eb520ecfe621c88)
    Reviewed-on: https://gerrit.mot.com/1296060
    Reviewed-by: Hujun Liao <liaohj@motorola.com>

--- drivers/misc/sx933x/sx933x_sar.c
+++ drivers/misc/sx933x/sx933x_sar.c
@@ -76,6 +76,7 @@ typedef struct sx933x
 static int irq_gpio_num;
 static psx93XX_t global_sx933x;
 
+static void sx93XX_schedule_work(psx93XX_t this, unsigned long delay);
 static int sx933x_get_nirq_state(void)
 {
 	return  !gpio_get_value(irq_gpio_num);
@@ -1146,8 +1147,8 @@ static int sx933x_suspend(struct device *dev)
 {
 	psx93XX_t this = dev_get_drvdata(dev);
 	if (this) {
+		sx933x_i2c_write_16bit(this,SX933X_CMD_REG,0xD);//make sx933x in Sleep mode
 		disable_irq(this->irq);
-		//sx933x_i2c_write_16bit(this,SX933X_CPS_CTRL0_REG,0x20);//make sx933x in Sleep mode
 	}
 	return 0;
 }
@@ -1156,8 +1157,9 @@ static int sx933x_resume(struct device *dev)
 {
 	psx93XX_t this = dev_get_drvdata(dev);
 	if (this) {
+		sx93XX_schedule_work(this,0);
 		enable_irq(this->irq);
-		//sx933x_i2c_write_16bit(this,SX933X_CPS_CTRL0_REG,0x27);//Exit from Sleep mode
+		sx933x_i2c_write_16bit(this,SX933X_CMD_REG,0xC);//Exit from Sleep mode
 	}
 	return 0;
 }
