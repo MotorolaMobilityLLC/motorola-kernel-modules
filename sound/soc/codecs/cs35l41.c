3589ca39b4ac3f6864acb246de767c9bd78d481f
David Rhodes <david.rhodes@cirrus.com>
2019-06-14 15:32:12 -0500

    ASoC: cs35l41: Hibernate Wake at DSP Unload
    Wake CS35L41 when unloading the HALO DSP
    
    CRs-fixed:(CR)
    
    Change-Id: I55fdc3ef4b57461dfe24de20115983bc56ce5f88
    Signed-off-by: David Rhodes <david.rhodes@cirrus.com>
    Reviewed-on: https://gerrit.mot.com/1476659
    Tested-by: Jira Key
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/codecs/cs35l41.c
+++ sound/soc/codecs/cs35l41.c
@@ -151,6 +151,10 @@ static const unsigned char cs35l41_bst_k2_table[4][5] = {
 static const unsigned char cs35l41_bst_slope_table[4] = {
 					0x75, 0x6B, 0x3B, 0x28};
 
+static int cs35l41_enter_hibernate(struct cs35l41_private *cs35l41);
+static int cs35l41_exit_hibernate(struct cs35l41_private *cs35l41);
+static int cs35l41_restore(struct cs35l41_private *cs35l41);
+
 static int cs35l41_dsp_power_ev(struct snd_soc_dapm_widget *w,
 		       struct snd_kcontrol *kcontrol, int event)
 {
@@ -167,6 +171,13 @@ static int cs35l41_dsp_power_ev(struct snd_soc_dapm_widget *w,
 		return 0;
 	case SND_SOC_DAPM_PRE_PMD:
 		if (cs35l41->halo_booted == false) {
+			cancel_delayed_work(&cs35l41->hb_work);
+			mutex_lock(&cs35l41->hb_lock);
+			cs35l41_exit_hibernate(cs35l41);
+			mutex_unlock(&cs35l41->hb_lock);
+
+			cs35l41->amp_hibernate = CS35L41_HIBERNATE_NOT_LOADED;
+
 			wm_halo_early_event(w, kcontrol, event);
 			wm_halo_event(w, kcontrol, event);
 		}
@@ -1133,10 +1144,6 @@ static const struct reg_sequence cs35l41_pdn_patch[] = {
 	{0x00000040, 0x000000CC},
 	{0x00000040, 0x00000033},
 };
-
-static int cs35l41_enter_hibernate(struct cs35l41_private *cs35l41);
-static int cs35l41_exit_hibernate(struct cs35l41_private *cs35l41);
-static int cs35l41_restore(struct cs35l41_private *cs35l41);
 
 static void cs35l41_hibernate_work(struct work_struct *work)
 {
