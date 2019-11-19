3fb1afb029eb5150df7ff8a443a3449b894c0be8
yaozm1 <yaozm1@lenovo.com>
2019-11-19 10:19:41 +0800

    audio/codec:Add AW882XX SmarPA Driver
    Porting aw882xx driver to bq-sh2019;
    Liber will use aw88288 as audio PA.
    
    Change-Id: I234c7be2fb2991947175a05be387f2837f159aa1
    Signed-off-by: yaozm1 <yaozm1@mt.com>
    Reviewed-on: https://gerrit.mot.com/1457370
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Reviewed-by: Yunfei Guo <guoyf2@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/codecs/Android.mk
+++ sound/soc/codecs/Android.mk
@@ -77,3 +77,9 @@ LOCAL_MODULE := tas2560_calib.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := snd_smartpa_aw882xx.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
