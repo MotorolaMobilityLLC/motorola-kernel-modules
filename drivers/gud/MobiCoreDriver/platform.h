3ed2ad23eac6b1fb3596ce662da088959f5f0de9
quebs2 <quebs2@motorola.com>
2020-09-08 15:54:29 +0800

    Update Trustonic TEE driver module to version 410a-V107
    Cherry-picked from (CR)
    Update the Trustonic TEE driver module with the version available in
    kinibi drop 410a-V107. This version contains the official patch to
    correctly select the silver CPU cores. A workaround was done in (CR),
    entitled "Force Trustonic TEE schedule run in Silver cores" and is being
    reverted in this change.
    
    Kinibi version t-base-QC8996-Android-410a-V107-20200907_110712_11106-Oem
    
    Change-Id: I70ebd67e7e49ce0153d089dc57fb2703a912bb54
    Reviewed-on: https://gerrit.mot.com/1949766
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Konstantin Makariev <kmakariev@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gud/MobiCoreDriver/platform.h
+++ drivers/gud/MobiCoreDriver/platform.h
@@ -115,9 +115,11 @@ static inline int smc_fastcall(void *fc_generic, size_t size)
  *	 "core_clk"
  *	 "iface_clk"
  */
-//#define MC_CRYPTO_CLOCK_MANAGEMENT
+#ifndef RSU_INTERNAL_CLOCK
+#define MC_CRYPTO_CLOCK_MANAGEMENT
+#endif
 /*
- * This should be defined only on platforms without ICE clock e.g SDM845, SM6150
+ * This should be defined only on platforms without ICE clock e.g SDM845, SM6150, SM7225
  * #define TT_CRYPTO_NO_CLOCK_SUPPORT_FEATURE "qcom,no-clock-support"
  */
 #define MC_CRYPTO_CLOCK_CORESRC_PROPNAME "qcom,ce-opp-freq"
@@ -135,23 +137,17 @@ static inline int smc_fastcall(void *fc_generic, size_t size)
 /* All TZBSPv4 targets are using AARCH32_FC flag */
 #define MC_AARCH32_FC
 
-/* This should be defined only on some platforms e.g SM8150, SM6150
+/* This should be defined only on some platforms e.g SM8150, SM6150, SM7225
  * from which the Gold cores do not support TEE interfaces
  * so that CPU_IDS should list only Silver cores.
  */
-#define CPU_SELECTION
-#if defined CPU_SELECTION
-#define NB_CPU 4
-#define CPU_IDS {0x0, 0x1, 0x2, 0x3}
-#endif
-
 /* Enforce/restrict statically CPUs potentially running TEE
  * (Customize to match platform CPU layout... 0xF0 for big cores only for ex).
  * If not defined TEE dynamically using all platform CPUs (recommended)
  * Warning: Both PLAT_DEFAULT_TEE_AFFINITY_MASK and
  * BIG_CORE_SWITCH_AFFINITY_MASK have to be defined
  */
-//#define PLAT_DEFAULT_TEE_AFFINITY_MASK (0xF)
-//#define BIG_CORE_SWITCH_AFFINITY_MASK (0xF)
+#define PLAT_DEFAULT_TEE_AFFINITY_MASK (0xF)
+#define BIG_CORE_SWITCH_AFFINITY_MASK (0xF)
 
 #endif /* _MC_PLATFORM_H_ */
