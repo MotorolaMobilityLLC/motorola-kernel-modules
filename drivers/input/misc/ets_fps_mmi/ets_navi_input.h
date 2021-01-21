ac34380e94e7339b8bbc8b1402f9946235ccf660
wuwl10 <wuwl10@lenovo.com>
2021-01-21 16:51:55 +0800

    dlkm: update ets fps driver
    Update the ets fps driver on MTK platform.
    It is different from qcom.
    
    Change-Id: Ia1b51099ad94bc0326ffd57cf4f3cb11093dc7b4
    Signed-off-by: wuwl10 <wuwl10@mt.com>
    Reviewed-on: https://gerrit.mot.com/1857593
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/misc/ets_fps_mmi/ets_navi_input.h
+++
@@ -1,6 +0,0 @@
-
-
-void uinput_egis_init(struct etspi_data *etspi);
-void uinput_egis_destroy(struct etspi_data *etspi);
-void sysfs_egis_init(struct etspi_data *etspi);
-void sysfs_egis_destroy(struct etspi_data *etspi);
