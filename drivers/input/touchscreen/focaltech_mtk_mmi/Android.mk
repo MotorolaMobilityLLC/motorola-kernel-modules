625d45ee28f7d999b25f8e27e30cc6d6b1b81882
wangyq13 <wangyq13@lenovo.com>
2021-03-25 13:53:17 +0800

    input/focal_mtk_mmi: move ft3518u driver to ko
    input/focal_mtk_mmi:
    Move ft3518u mtk driver to ko.
    Add single tap function.
    
    Change-Id: I0724c8ed292933fde627dcd7b643e27dd4755c3d
    Signed-off-by: wangyq13 <wangyq13@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1909534
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Wei Deng <dengwei1@motorola.com>
    Reviewed-by: Jun Weng <wengjun1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/input/touchscreen/focaltech_mtk_mmi/Android.mk
@@ -0,0 +1,24 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(BOARD_USES_DOUBLE_TAP),)
+	KERNEL_CFLAGS += CONFIG_INPUT_FOCALTECH_MMI_ENABLE_DOUBLE_TAP=y
+endif
+
+ifneq ($(BOARD_USES_TOUCH_PALM),)
+	KERNEL_CFLAGS += CONFIG_INPUT_FOCALTECH_MMI_ENABLE_PALM=y
+endif
+
+ifneq ($(BOARD_USES_PEN_NOTIFIER),)
+	KERNEL_CFLAGS += CONFIG_INPUT_FOCALTECH_MMI_PEN_NOTIFIER=y
+endif
+
+ifneq ($(findstring touchscreen_mmi.ko,$(BOARD_VENDOR_KERNEL_MODULES)),)
+    KERNEL_CFLAGS += CONFIG_INPUT_TOUCHSCREEN_MMI=y
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := focaltech_mtk_mmi.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
