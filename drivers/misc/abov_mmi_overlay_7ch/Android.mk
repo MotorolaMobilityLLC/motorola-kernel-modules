160366114bb9f1495cefde6a0dfcc82193848544
wunan3 <wunan3@lenovo.com>
2021-04-16 00:18:39 +0800

    ellis:sar sensor:charger support
    WHY:support sar sensor on mtk_charger for abov
    HOW:add support flags
    
    Change-Id: Ib4cac8956c2d2ec9337f199d605059b67f905351
    Signed-off-by: wunan3 <wunan3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1925120
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jianguo Liu <liujg6@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/abov_mmi_overlay_7ch/Android.mk
+++ drivers/misc/abov_mmi_overlay_7ch/Android.mk
@@ -1,8 +1,13 @@
 DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(ABOV_MTK_CHARGER),true)
+    KERNEL_CFLAGS  += CONFIG_ABOV_MTK_CHARGER=y
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := abov_sar_mmi_overlay_7ch.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
+
--- drivers/misc/abov_mmi_overlay_7ch/abov_sar_mmi_overlay_7ch.c
+++ drivers/misc/abov_mmi_overlay_7ch/abov_sar_mmi_overlay_7ch.c
@@ -1564,8 +1564,12 @@ static int ps_get_state(struct power_supply *psy, bool *present)
 	union power_supply_propval pval = { 0 };
 	int retval;
 
-	retval = power_supply_get_property(psy, POWER_SUPPLY_PROP_PRESENT,
+#ifdef CONFIG_ABOV_MTK_CHARGER
+	retval = power_supply_get_property(psy, POWER_SUPPLY_PROP_ONLINE,
 			&pval);
+#else
+	retval = power_supply_get_property(psy, POWER_SUPPLY_PROP_PRESENT,
+#endif
 	if (retval) {
 		LOG_ERR("%s psy get property failed\n", psy->desc->name);
 		return retval;
