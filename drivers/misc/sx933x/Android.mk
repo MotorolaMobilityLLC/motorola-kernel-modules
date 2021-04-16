ea3896422d9afc4856d39a9f3bdb5a8ee042487c
zhangh12 <zhangh12@motorola.com>
2021-04-16 14:16:07 +0800

    sar: can't reset after usb is plugged
    1. use the correct sys name of usb node on MTK
    2. AndroidKernelModule.mk should be included behind MACRO
    
    Change-Id: I363a08806705aed23a16bfc2ccc5552308e8a044
    Signed-off-by: zhangh12 <zhangh12@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1927026
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wen Xie <xiewen3@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/sx933x/Android.mk
+++ drivers/misc/sx933x/Android.mk
@@ -1,11 +1,9 @@
 DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := sx933x_sar.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-include $(DLKM_DIR)/AndroidKernelModule.mk
+ifeq ($(SX933X_MTK_CHARGER),true)
+        KERNEL_CFLAGS += CONFIG_SX933X_MTK_CHARGER=y
+endif
 
 ifeq ($(SX933X_USB_CAL),true)
 	KERNEL_CFLAGS += CONFIG_SX933X_USB_CAL=y
@@ -14,3 +12,9 @@ endif
 ifeq ($(SX933X_FLIP_CAL),true)
 	KERNEL_CFLAGS += CONFIG_SX933X_FLIP_CAL=y
 endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := sx933x_sar.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
