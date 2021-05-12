bda618a4aa88e3b439ac607a77a5ee64f6fba1a9
huaqin66 <huaqin66@motorola.com>
2021-05-12 16:49:48 +0800

    motorola/drivers:add new manage for new TD
    add new manage for new TD
    lisbon
    
    Change-Id: If4fe8c5f4f5060538a513a622b73ed22bcf7b2e1
    Signed-off-by: huaqin66 <huaqin66@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1946825
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wei Xu <xuwei9@lenovo.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/mmi_parallel_charger/pps.c
+++ drivers/power/mmi_parallel_charger/pps.c
@@ -59,6 +59,69 @@ int usbpd_select_pdo(struct mmi_charger_manager *chip, u32 mV, u32 mA)
 		return 0;
 }
 
+bool usbpd_get_pps_status_curr_volt(struct mmi_charger_manager *chip)
+{
+	int ret, apdo_idx = -1;
+	struct tcpm_power_cap_val apdo_cap = {0};
+	struct tcpm_power_cap_val selected_apdo_cap;
+	u8 cap_idx;
+
+	if (check_typec_attached_snk(chip->tcpc) < 0)
+		return false;
+
+	if (!chip->is_pps_en_unlock) {
+		pr_err("pps en is locked\n");
+		return false;
+	}
+
+	if (!tcpm_inquire_pd_pe_ready(chip->tcpc)) {
+		pr_err("PD PE not ready\n");
+		return false;
+	}
+
+	cap_idx = 0;
+	while (1) {
+		ret = tcpm_inquire_pd_source_apdo(chip->tcpc,
+						  TCPM_POWER_CAP_APDO_TYPE_PPS,
+						  &cap_idx, &apdo_cap);
+		if (ret != TCP_DPM_RET_SUCCESS) {
+			pr_err("inquire pd apdo fail(%d)\n", ret);
+			break;
+		}
+
+		pr_err("cap_idx[%d], %d mv ~ %d mv, %d ma, pl: %d\n", cap_idx,
+			 apdo_cap.min_mv, apdo_cap.max_mv, apdo_cap.ma,
+			 apdo_cap.pwr_limit);
+
+		if (apdo_cap.max_mv < PUMP_CHARGER_PPS_MIN_VOLT / 1000||
+			apdo_cap.ma <  chip->typec_middle_current / 1000)
+				continue;
+
+		if (apdo_idx == -1 || apdo_cap.ma > selected_apdo_cap.ma) {
+			memcpy(&selected_apdo_cap, &apdo_cap,
+			       sizeof(struct tcpm_power_cap_val));
+			apdo_idx = cap_idx;
+			pr_err("select potential cap_idx[%d]\n", cap_idx);
+		}
+	}
+
+    if (apdo_idx != -1) {
+		if (selected_apdo_cap.max_mv* 1000 < pd_volt_max_init) {
+			chip->pd_volt_max = selected_apdo_cap.max_mv* 1000;
+		} else {
+			chip->pd_volt_max = pd_volt_max_init;
+		}
+		if (selected_apdo_cap.ma* 1000 < pd_curr_max_init) {
+			chip->pd_curr_max = selected_apdo_cap.ma * 1000;
+		} else {
+			chip->pd_curr_max = pd_curr_max_init;
+		}
+		return true;
+    }
+
+    return false;
+}
+
 bool usbpd_get_pps_status(struct mmi_charger_manager *chip)
 {
 	int ret, apdo_idx = -1;
