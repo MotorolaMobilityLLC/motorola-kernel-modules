61f14e0d26ff2c836ba2c7e75b98d76d054f5fc9
hujing3 <hujing3@lenovo.com>
2021-05-31 15:16:37 +0800

    focaltech_mtk_v2_mmi: add panel supplier
    focaltech touch driver:
    support panel supplier
    
    Change-Id: I3e2e744b294161f7f7033582877f88a53c185533
    Signed-off-by: hujing3 <hujing3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1965260
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/focaltech_mtk_v2_mmi/focaltech_ex_fun.c
+++ drivers/input/touchscreen/focaltech_mtk_v2_mmi/focaltech_ex_fun.c
@@ -1146,6 +1146,18 @@ static ssize_t fts_log_level_store(
     return count;
 }
 
+static ssize_t fts_panel_supplier_show(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+	struct fts_ts_data *data = dev_get_drvdata(dev);
+
+	if (data->panel_supplier)
+		return scnprintf(buf, PAGE_SIZE, "%s\n",
+			data->panel_supplier);
+	return 0;
+}
+static DEVICE_ATTR(panel_supplier, 0444, fts_panel_supplier_show, NULL);
+
 /* get the fw version  example:cat fw_version */
 static DEVICE_ATTR(fts_fw_version, S_IRUGO | S_IWUSR, fts_tpfwver_show, fts_tpfwver_store);
 
@@ -1187,6 +1199,7 @@ static struct attribute *fts_attributes[] = {
     &dev_attr_fts_boot_mode.attr,
     &dev_attr_fts_touch_point.attr,
     &dev_attr_fts_log_level.attr,
+    &dev_attr_panel_supplier.attr,
     NULL
 };
 
