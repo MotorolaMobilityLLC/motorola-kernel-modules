0b66dea7bec9f04c2e051c3c1b8b39d28e9690e9
huaqin66 <huaqin66@motorola.com>
2021-07-06 13:55:53 +0800

    motorola/drivers: change battery para and thermal.conf[2/3]
    change battery para and thermal.conf
    lisbon
    
    Change-Id: I78fb5fac9424dfe1fe10eb9172eb3ba1fecba80d
    Signed-off-by: huaqin66 <huaqin66@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2003201
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Lei Chen <chenl18@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/mmi_parallel_charger/mmi_charger_core.h
+++ drivers/power/mmi_parallel_charger/mmi_charger_core.h
@@ -155,6 +155,7 @@ struct mmi_charger_manager {
 	bool			awake;
 	bool			cp_disable;
 	bool			sourcecap_dec_enable;
+	bool			disable_ignore_hysteresis;
 
 	int *debug_mask;
 	int pps_volt_steps;	/*PPS voltage, programming step size*/
