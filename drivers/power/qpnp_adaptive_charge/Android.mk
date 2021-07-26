87a9c8bad5916a16fff3a7cd4d9e52f8344cbaa2
xuwei9 <xuwei9@lenovo.com>
2021-07-26 11:07:47 +0800

    charger:adaptive charger tolerance optimization
    Adaptive charger tolerance optimization
    from 1 to 2 for more stable state switching.
    
    Change-Id: I60a0c5501605ac060919d340a07b2f54faa0907c
    Signed-off-by: xuwei9 <xuwei9@mt.com>
    Reviewed-on: https://gerrit.mot.com/2023609
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/2023789
    Reviewed-by: Suting Xie <xiest1@motorola.com>
    SLTApproved: Suting Xie <xiest1@motorola.com>
    Submit-Approved: Suting Xie <xiest1@motorola.com>
    Tested-by: Suting Xie <xiest1@motorola.com>
    Reviewed-by: lingsen2 <lingsen2@motorola.com>

--- drivers/power/qpnp_adaptive_charge/Android.mk
+++ drivers/power/qpnp_adaptive_charge/Android.mk
@@ -4,6 +4,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := qpnp_adaptive_charge.ko
 LOCAL_MODULE_TAGS := optional
+
+ifeq ($(ADAPTIVE_TOLERANCE_OPTIMIZATION),true)
+	KERNEL_CFLAGS += ADAPTIVE_TOLERANCE_OPTIMIZATION=y
+endif
+
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
