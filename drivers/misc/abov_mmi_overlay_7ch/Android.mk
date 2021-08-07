ed29d1a386a750cd938c217290c75179a3ff435f
wunan3 <wunan3@lenovo.com>
2021-08-07 22:41:36 +0800

    ellis:Cap sense Force Calibration Issue
    WHY:ellis use MTK charger type
    HOW:add compile flag
    
    Change-Id: I59f87718929b6c3545fcbd4bdc708423ff0f02c5
    Signed-off-by: wunan3 <wunan3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2035976
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yuchang Guo <guoyc1@lenovo.com>
    Reviewed-by: Wang Wang <wangwang1@lenovo.com>
    Submit-Approved: Jira Key

--- drivers/misc/abov_mmi_overlay_7ch/Android.mk
+++ drivers/misc/abov_mmi_overlay_7ch/Android.mk
@@ -5,6 +5,10 @@ ifeq ($(ABOV_MTK_CHARGER),true)
     KERNEL_CFLAGS  += CONFIG_ABOV_MTK_CHARGER=y
 endif
 
+ifeq ($(ABOV_MTK_KERNEL419_CHARGER_TYPE), true)
+    KERNEL_CFLAGS  += CONFIG_ABOV_MTK_KERNEL419_CHARGER_TYPE=y
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := abov_sar_mmi_overlay_7ch.ko
 LOCAL_MODULE_TAGS := optional
