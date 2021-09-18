e369f3086ab0374b09d1b548bdfc9378a9b58ab4
hujing3 <hujing3@lenovo.com>
2021-09-18 10:06:32 +0800

    goodix_mtk_gtx8_mmi: enable gesture module
    goodix_mtk_gtx8_mmi:
    enable gesture module and support single tap
    
    Change-Id: Id912b65c17378f556885f1356d64b81fa35bbd29
    Signed-off-by: hujing3 <hujing3@mt.com>
    Reviewed-on: https://gerrit.mot.com/2070509
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/goodix_mtk_gtx8_mmi/Android.mk
+++ drivers/input/touchscreen/goodix_mtk_gtx8_mmi/Android.mk
@@ -13,3 +13,9 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := goodix_mtk_gtx8_gesture_mmi.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
