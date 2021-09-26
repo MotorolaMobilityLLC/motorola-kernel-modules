80a436b847f048b95a73510d301c1cb2ab4f92b1
hujing3 <hujing3@lenovo.com>
2021-09-26 11:00:20 +0800

    goodix_mtk_gtx8_mmi:support control single tap from app setting
    goodix_mtk_gtx8_mmi:
    support to control single tap function from app setting
    If single tap option is enabled, create a virtual sensor for app
    
    Change-Id: I92fcc82f6330396edf91d15d5bc987f912eb4e23
    Reviewed-on: https://gerrit.mot.com/2082268
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wang Wang <wangwang1@lenovo.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/goodix_mtk_gtx8_mmi/Android.mk
+++ drivers/input/touchscreen/goodix_mtk_gtx8_mmi/Android.mk
@@ -5,6 +5,10 @@ ifeq ($(CONFIG_INPUT_FB_PANEL), true)
 	KERNEL_CFLAGS += CONFIG_GOODIX_FB_PANEL=y
 endif
 
+ifneq ($(BOARD_USES_DOUBLE_TAP),)
+	KERNEL_CFLAGS += CONFIG_INPUT_GTX_MTK_MMI_ENABLE_DOUBLE_TAP=y
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := goodix_mtk_gtx8_mmi.ko
 LOCAL_MODULE_TAGS := optional
