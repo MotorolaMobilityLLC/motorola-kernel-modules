f1aa53fee910475a9f37ed4cacafc9c844c73d27
hujing3 <hujing3@lenovo.com>
2021-10-04 15:27:25 +0800

    goodix_mtk_gtx8_mmi:add FB panel check when probe for multi panels
    goodix_mtk_gtx8_mmi:
    add FB panel check for multi panels when probe
    to avoid potential conflicts
    
    Change-Id: I49301e5b7d293fd7a8e88d26589716ecc62ba462
    Signed-off-by: hujing3 <hujing3@mt.com>
    Reviewed-on: https://gerrit.mot.com/2082267
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wang Wang <wangwang1@mt.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/goodix_mtk_gtx8_mmi/Android.mk
+++ drivers/input/touchscreen/goodix_mtk_gtx8_mmi/Android.mk
@@ -1,6 +1,10 @@
 DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(CONFIG_INPUT_FB_PANEL), true)
+	KERNEL_CFLAGS += CONFIG_GOODIX_FB_PANEL=y
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := goodix_mtk_gtx8_mmi.ko
 LOCAL_MODULE_TAGS := optional
