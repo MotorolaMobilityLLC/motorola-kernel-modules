04c50563cc0f20a0ddeee56b05a610c2d4dbd723
weimh1 <weimh1@lenovo.com>
2021-11-21 17:46:44 +0800

    tesla: ko build system
      MTK S is different from R build about ko.
      We must make some changes to compatible it.
      1.AndroidKernelModule.mk to ajust path
      2.moto ko Android.mk to fix dependency
    
    Change-Id: I891b30f9f4db33926c660c8b81848ee113d089d0
    Signed-off-by: weimh1 <weimh1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2122838
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Lianlu Chen <chenll4@lenovo.com>
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/goodix_berlin_mmi/goodix_brl_ext/Android.mk
+++ drivers/input/touchscreen/goodix_berlin_mmi/goodix_brl_ext/Android.mk
@@ -6,6 +6,7 @@ LOCAL_MODULE := goodix_brl_tools_mmi.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 LOCAL_ADDITIONAL_DEPENDENCIES += $(KERNEL_MODULES_OUT)/goodix_brl_mmi.ko
+LOCAL_REQUIRED_MODULES := goodix_brl_mmi.ko
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
@@ -13,4 +14,5 @@ LOCAL_MODULE := goodix_brl_gesture_mmi.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 LOCAL_ADDITIONAL_DEPENDENCIES += $(KERNEL_MODULES_OUT)/goodix_brl_mmi.ko
+LOCAL_REQUIRED_MODULES := goodix_brl_mmi.ko
 include $(DLKM_DIR)/AndroidKernelModule.mk
--- drivers/input/touchscreen/synaptics_tcm_mmi_v2/synaptics_core/Android.mk
+++ drivers/input/touchscreen/synaptics_tcm_mmi_v2/synaptics_core/Android.mk
@@ -18,11 +18,14 @@ LOCAL_MODULE_PATH := $(SYNA_TCM_MODULE_PATH)
 ifneq ($(findstring touchscreen_mmi.ko,$(BOARD_VENDOR_KERNEL_MODULES)),)
     KERNEL_CFLAGS += CONFIG_INPUT_TOUCHSCREEN_MMI=y
     LOCAL_ADDITIONAL_DEPENDENCIES += $(KERNEL_MODULES_OUT)/touchscreen_mmi.ko
+    LOCAL_REQUIRED_MODULES := touchscreen_mmi.ko
 endif
 ifneq ($(findstring synaptics_i2c.ko,$(BOARD_VENDOR_KERNEL_MODULES)),)
     LOCAL_ADDITIONAL_DEPENDENCIES += $(KERNEL_MODULES_OUT)/synaptics_i2c.ko
+    LOCAL_REQUIRED_MODULES := synaptics_i2c.ko
 else
     LOCAL_ADDITIONAL_DEPENDENCIES += $(KERNEL_MODULES_OUT)/synaptics_spi.ko
+    LOCAL_REQUIRED_MODULES := synaptics_spi.ko
 endif
 
 KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
