e3a115ef577b8fcc5c742c09ba7acde168599905
wuwl10 <wuwl10@lenovo.com>
2022-02-23 15:35:53 +0800

    dlkm: aw86715: update vibrator driver
    1. Update driver to MTK V1.0.0
    2. Change the default trigger register to make it work
    on use build since activate/duration/state will be register
    by led-trigger.c
    3. Change the sampling rate as 12K
    
    Change-Id: Ibaf5e1e7c799600d97b42a9f22be26ccdd1677b2
    Signed-off-by: wuwl10 <wuwl10@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2198682
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/awinic/aw86715/haptic_hv.h
+++ drivers/misc/awinic/aw86715/haptic_hv.h
@@ -6,6 +6,7 @@
 #include <linux/workqueue.h>
 #include <linux/hrtimer.h>
 #include <linux/mutex.h>
+#include <linux/input.h>
 #include <linux/cdev.h>
 #include <linux/version.h>
 #include <sound/control.h>
@@ -56,11 +57,13 @@
 #define AW_I2C_BYTE_EIGHT			(8)
 
 #define AW_SEQUENCER_LOOP_SIZE			(4)
+#define AW_RAM_GET_F0_SEQ			(8)
 #define AW_RTP_NAME_MAX				(64)
-//#define AW_PM_QOS_VALUE_VB			(400)
-#define CPU_LATENCY_QOC_VALUE                   (0)
+#define AW_PM_QOS_VALUE_VB			(400)
+#define AW_DRV2_LVL_MAX				(0x7F)
 #define AW_VBAT_REFER				(4200)
 #define AW_VBAT_MIN				(3000)
+#define AW_VBAT_MAX				(4500)
 #define AW_RAM_WORK_DELAY_INTERVAL		(8000)
 #define AW_OSC_TRIM_PARAM			(50)
 #define AW_OSC_CALI_ACCURACY			(24)
@@ -68,18 +71,21 @@
 #define AW_TRIG_NUM				(3)
 #define AW_RAMDATA_RD_BUFFER_SIZE		(1024)
 #define AW_RAMDATA_WR_BUFFER_SIZE		(2048)
-
+#define AW_EFFECT_ID_BOUNDS			(4)
+#define AW_EFFECT_NUMBER			(3)
 #define AW_GLBRD_STATE_MASK			(15<<0)
 #define AW_STATE_STANDBY			(0x00)
 #define AW_STATE_RTP				(0x08)
 #define AW_BIT_RESET				(0xAA)
-#define AW_I2C_ADDR				(0x5A)
+#define CPU_LATENCY_QOC_VALUE			(0)
 
 #define AW_CHECK_RAM_DATA
 #define AW_READ_BIN_FLEXBALLY
-#define AW_CHECK_QUALIFY
+#define AW_LRA_F0_DEFAULT
+#define AW_INPUT_FRAMEWORK
+/* #define AW_TIKTAP */
 /* #define AW_DOUBLE */
-#define AW_DURATION_DECIDE_WAVEFORM
+/* #define AW_DURATION_DECIDE_WAVEFORM */
 /* #define AW_OSC_MULTI_CALI */
 /* #define AW_BOOT_OSC_CALI */
 /* #define AW_ENABLE_RTP_PRINT_LOG */
@@ -90,19 +96,76 @@
 #define AW869XX_DRIVER_ENABLE
 #define AW8671X_DRIVER_ENABLE
 #define AW8692X_DRIVER_ENABLE
-#define AW_ENABLE_PIN_CONTROL
 
 /*********************************************************
  *
  * Conditional Marco
  *
  *********************************************************/
+
+#ifdef AW_TIKTAP
+#define AW_TIKTAP_PROCNAME			"tiktap_buf"
+#define AW_TIKTAP_MMAP_PAGE_ORDER		(2)
+#define AW_TIKTAP_MMAP_BUF_SUM			(16)
+#define AW_TIKTAP_MMAP_BUF_SIZE			(1000)
+
+#pragma pack(4)
+struct mmap_buf_format {
+	uint8_t status;
+	uint8_t bit;
+	int16_t length;
+
+	struct mmap_buf_format *kernel_next;
+	struct mmap_buf_format *user_next;
+	uint8_t reg_addr;
+	int8_t data[AW_TIKTAP_MMAP_BUF_SIZE];
+}; /* 1024 byte */
+#pragma pack()
+
+#define TIKTAP_IOCTL_GROUP		0xFF
+#define TIKTAP_GET_F0			_IO(TIKTAP_IOCTL_GROUP, 0x01)
+#define TIKTAP_GET_HWINFO		_IO(TIKTAP_IOCTL_GROUP, 0x02)
+#define TIKTAP_SET_FREQ			_IO(TIKTAP_IOCTL_GROUP, 0x03)
+#define TIKTAP_SETTING_GAIN		_IO(TIKTAP_IOCTL_GROUP, 0x04)
+#define TIKTAP_SETTING_SPEED		_IO(TIKTAP_IOCTL_GROUP, 0x05)
+#define TIKTAP_SETTING_BSTVOL		_IO(TIKTAP_IOCTL_GROUP, 0x06)
+#define TIKTAP_ON_MODE			_IO(TIKTAP_IOCTL_GROUP, 0x07)
+#define TIKTAP_OFF_MODE			_IO(TIKTAP_IOCTL_GROUP, 0x08)
+#define TIKTAP_RTP_MODE			_IO(TIKTAP_IOCTL_GROUP, 0x09)
+#define TIKTAP_RTP_IRQ_MODE		_IO(TIKTAP_IOCTL_GROUP, 0x0A)
+#define TIKTAP_STOP_MODE		_IO(TIKTAP_IOCTL_GROUP, 0x0B)
+#define TIKTAP_STOP_RTP_MODE		_IO(TIKTAP_IOCTL_GROUP, 0x0C)
+#define TIKTAP_WRITE_REG		_IO(TIKTAP_IOCTL_GROUP, 0x0D)
+#define TIKTAP_READ_REG			_IO(TIKTAP_IOCTL_GROUP, 0x0E)
+#define TIKTAP_BST_SWITCH		_IO(TIKTAP_IOCTL_GROUP, 0x0F)
+#define TIKTAP_GET_SPEED		_IO(TIKTAP_IOCTL_GROUP, 0x10)
+
+enum {
+	MMAP_BUF_DATA_VALID = 0x55,
+	MMAP_BUF_DATA_FINISHED = 0xAA,
+	MMAP_BUF_DATA_INVALID = 0xFF,
+};
+
+#define TIkTAP_LEFT_STATUS_MASK		(0x0F)
+#define TIkTAP_LEFT_FINISHED_DONE	(MMAP_BUF_DATA_FINISHED&0xF0)
+
+#define TIKTAP_RIGHT_STATUS_MASK	(0xF0)
+#define TIKTAP_RIGHT_FINISHED_DONE	(MMAP_BUF_DATA_FINISHED&0x0F)
+
+#define TIKTAP_L_VALID_R_FINISHED	((MMAP_BUF_DATA_VALID&TIKTAP_RIGHT_STATUS_MASK)|TIKTAP_RIGHT_FINISHED_DONE)
+#define TIKTAP_R_VALID_L_FINISHED	((MMAP_BUF_DATA_VALID&TIkTAP_LEFT_STATUS_MASK)|TIkTAP_LEFT_FINISHED_DONE)
+#endif
+
 #if LINUX_VERSION_CODE <= KERNEL_VERSION(4, 4, 1)
 #define TIMED_OUTPUT
 #endif
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 19, 1)
 #define KERNEL_OVER_4_19
+#endif
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
+#define KERNEL_OVER_5_10
 #endif
 
 #ifdef TIMED_OUTPUT
@@ -137,51 +200,40 @@ struct aw_componet_codec_ops {
  * AW869X
  *
  *********************************************************/
-#define AW869X_TRG1_ENABLE			(1)
-#define AW869X_TRG2_ENABLE			(1)
-#define AW869X_TRG3_ENABLE			(1)
-#define AW869X_TRG1_DISABLE			(0)
-#define AW869X_TRG2_DISABLE			(0)
-#define AW869X_TRG3_DISABLE			(0)
-#define AW869X_TRG1_DUAL_EDGE			(1)
-#define AW869X_TRG2_DUAL_EDGE			(1)
-#define AW869X_TRG3_DUAL_EDGE			(1)
-#define AW869X_TRG1_DEFAULT_LEVEL		(1)
-#define AW869X_TRG2_DEFAULT_LEVEL		(1)
-#define AW869X_TRG3_DEFAULT_LEVEL		(1)
-#define AW869X_TRG1_FIRST_EDGE_SEQ		(1)
-#define AW869X_TRG1_SECOND_EDGE_SEQ		(2)
-#define AW869X_TRG2_FIRST_EDGE_SEQ		(1)
-#define AW869X_TRG2_SECOND_EDGE_SEQ		(2)
-#define AW869X_TRG3_FIRST_EDGE_SEQ		(1)
-#define AW869X_TRG3_SECOND_EDGE_SEQ		(2)
-#define AW869X_VBAT_MAX				(4500)
-#define AW869X_MUL_GET_F0_RANGE			(100)
 #define AW869X_MUL_GET_F0_NUM			(3)
-#define AW869X_LRA_FORMULA(lra_code)		(298 * lra_code)
-#define AW869X_VBAT_FORMULA(vbat_code)		(6100 * vbat_code / 256)
-#define AW869X_RAM_ADDR_H(base_addr)		(base_addr >> 8)
-#define AW869X_RAM_ADDR_L(base_addr)		(base_addr & 0x00FF)
-#define AW869X_FIFO_AE_ADDR_H(base_addr)	((base_addr >> 1) >> 8)
-#define AW869X_FIFO_AE_ADDR_L(base_addr)	((base_addr >> 1) & 0x00ff)
-#define AW869X_FIFO_AF_ADDR_H(base_addr)	((base_addr - (base_addr >> 2)) >> 8)
-#define AW869X_FIFO_AF_ADDR_L(base_addr)	((base_addr - (base_addr >> 2)) & 0x00ff)
+#define AW869X_MUL_GET_F0_RANGE			(100)
+#define AW869X_BST_VOL_MIN			(6000)
+#define AW869X_BST_VOL_MAX			(10500)
+#define AW869X_BST_VOL_FARMULA(bst_vol)		((((bst_vol) - 6000) / 142) << 1)
+#define AW869X_F0_FARMULA(f0_reg, coeff)	(1000000000 / ((f0_reg) * (coeff)))
+#define AW869X_LRA_FORMULA(lra_code)		(298 * (lra_code))
+#define AW869X_VBAT_FORMULA(vbat_code)		(6100 * (vbat_code) / 256)
+#define AW869X_RAM_ADDR_H(base_addr)		((base_addr) >> 8)
+#define AW869X_RAM_ADDR_L(base_addr)		((base_addr) & 0x00FF)
+#define AW869X_FIFO_AE_ADDR_H(base_addr)	(((base_addr) >> 1) >> 8)
+#define AW869X_FIFO_AE_ADDR_L(base_addr)	(((base_addr) >> 1) & 0x00ff)
+#define AW869X_FIFO_AF_ADDR_H(base_addr)	(((base_addr) - ((base_addr) >> 2)) >> 8)
+#define AW869X_FIFO_AF_ADDR_L(base_addr)	(((base_addr) - ((base_addr) >> 2)) & 0x00ff)
 
 /*********************************************************
  *
  * AW869XX
  *
  *********************************************************/
-#define AW869XX_VBAT_MAX			(5500)
-#define AW869XX_LRA_FORMULA(lra_code)		((lra_code * 678 * 1000) / \
+#define AW869XX_BST_VOL_MIN			(6000)
+#define AW869XX_BST_VOL_MAX			(10971)
+#define AW869XX_DRV2_LVL_FARMULA(vrms)		(199 * (vrms) / 3050)
+#define AW869XX_BST_VOL_FARMULA(bst_vol)	(((bst_vol) - 6000) * 1000 / 78893)
+#define AW869XX_F0_FARMULA(f0_reg)		(384000 * 10 / (f0_reg))
+#define AW869XX_LRA_FORMULA(lra_code)		(((lra_code) * 678 * 1000) / \
 						(1024 * 10))
-#define AW869XX_VBAT_FORMULA(vbat_code)		(6100 * vbat_code / 1024)
-#define AW869XX_RAM_ADDR_H(base_addr)		(base_addr >> 8)
-#define AW869XX_RAM_ADDR_L(base_addr)		(base_addr & 0x00FF)
-#define AW869XX_FIFO_AE_ADDR_H(base_addr)	(((base_addr >> 1) >> 4) & 0xF0)
-#define AW869XX_FIFO_AE_ADDR_L(base_addr)	((base_addr >> 1) & 0x00ff)
-#define AW869XX_FIFO_AF_ADDR_H(base_addr)	(((base_addr - (base_addr >> 2)) >> 8) & 0x0F)
-#define AW869XX_FIFO_AF_ADDR_L(base_addr)	((base_addr - (base_addr >> 2)) & 0x00ff)
+#define AW869XX_VBAT_FORMULA(vbat_code)		(6100 * (vbat_code) / 1024)
+#define AW869XX_RAM_ADDR_H(base_addr)		((base_addr) >> 8)
+#define AW869XX_RAM_ADDR_L(base_addr)		((base_addr) & 0x00FF)
+#define AW869XX_FIFO_AE_ADDR_H(base_addr)	((((base_addr) >> 1) >> 4) & 0xF0)
+#define AW869XX_FIFO_AE_ADDR_L(base_addr)	(((base_addr) >> 1) & 0x00ff)
+#define AW869XX_FIFO_AF_ADDR_H(base_addr)	((((base_addr) - ((base_addr) >> 2)) >> 8) & 0x0F)
+#define AW869XX_FIFO_AF_ADDR_L(base_addr)	(((base_addr) - ((base_addr) >> 2)) & 0x00ff)
 #define AW869XX_RATES				SNDRV_PCM_RATE_8000_48000
 #define AW869XX_FORMATS				(SNDRV_PCM_FMTBIT_S16_LE | \
 						SNDRV_PCM_FMTBIT_S24_LE | \
@@ -192,15 +244,14 @@ struct aw_componet_codec_ops {
  * AW8671X
  *
  *********************************************************/
-#define AW8671X_VBAT_MAX			(5500)
-#define AW8671X_F0_FARMULA(f0_reg)		(384000 * 10 / f0_reg)
-#define AW8671X_VBAT_FORMULA(vbat)		(6115 * vbat / 4096)
-#define AW8671X_LRA_FORMULA(lra, d2s_gain)	((1210 * 1000 * 10 * lra) / \
-						(2047 * 36 * d2s_gain))
-#define AW8671X_FIFO_AE_ADDR_H(base_addr)	(((base_addr >> 1) >> 4) & 0xF0)
-#define AW8671X_FIFO_AE_ADDR_L(base_addr)	((base_addr >> 1) & 0x00ff)
-#define AW8671X_FIFO_AF_ADDR_H(base_addr)	((base_addr - (base_addr >> 2)) >> 8 & 0x0F)
-#define AW8671X_FIFO_AF_ADDR_L(base_addr)	((base_addr - (base_addr >> 2)) & 0x00ff)
+#define AW8671X_DRV2_LVL_FARMULA(vrms)		(199 * (vrms) / 4000)
+#define AW8671X_F0_FARMULA(f0_reg)		(384000 * 10 / (f0_reg))
+#define AW8671X_VBAT_FORMULA(vbat)		(6050 * (vbat) / 4095)
+#define AW8671X_LRA_FORMULA(lra, d2s_gain)	((164197 * (lra)) / (1000 * (d2s_gain)))
+#define AW8671X_FIFO_AE_ADDR_H(base_addr)	((((base_addr) >> 1) >> 4) & 0xF0)
+#define AW8671X_FIFO_AE_ADDR_L(base_addr)	(((base_addr) >> 1) & 0x00ff)
+#define AW8671X_FIFO_AF_ADDR_H(base_addr)	(((base_addr) - ((base_addr) >> 2)) >> 8 & 0x0F)
+#define AW8671X_FIFO_AF_ADDR_L(base_addr)	(((base_addr) - ((base_addr) >> 2)) & 0x00ff)
 #define AW8671X_RATES				SNDRV_PCM_RATE_8000_48000
 #define AW8671X_FORMATS				(SNDRV_PCM_FMTBIT_S16_LE | \
 						SNDRV_PCM_FMTBIT_S24_LE | \
@@ -211,18 +262,26 @@ struct aw_componet_codec_ops {
  * AW8692X
  *
  *********************************************************/
-#define AW8692X_VBAT_MAX			(5500)
-#define AW8692X_LRA_FORMULA(lra, d2s_gain)	((6075 * 100 * lra) / \
-						(1024 * d2s_gain))
-#define AW8692X_VBAT_FORMULA(vbat)		(5 * 1215 * vbat / 1024)
-#define AW8692X_RAMADDR_H(base_addr)		(base_addr >> 8)
-#define AW8692X_RAMADDR_L(base_addr)		(base_addr & 0x00FF)
-#define AW8692X_BASEADDR_H(base_addr)		(base_addr >> 8)
-#define AW8692X_BASEADDR_L(base_addr)		(base_addr & 0x00FF)
-#define AW8692X_FIFO_AE_ADDR_H(base_addr)	(((base_addr >> 1) >> 4) & 0xF0)
-#define AW8692X_FIFO_AE_ADDR_L(base_addr)	((base_addr >> 1) & 0x00ff)
-#define AW8692X_FIFO_AF_ADDR_H(base_addr)	(((base_addr - (base_addr >> 2)) >> 8) & 0x0F)
-#define AW8692X_FIFO_AF_ADDR_L(base_addr)	((base_addr - (base_addr >> 2)) & 0x00ff)
+#define AW8692X_BST_VOL_MIN			(6000)
+#define AW8692X_BST_VOL_MAX			(11437)
+#define AW8692X_BOOT_DETECTION			(5)
+#define AW8692X_DRV2_LVL_FARMULA(vrms)		(199 * (vrms) / 4000)
+#define AW8692X_BST_VOL_FARMULA(bst_vol)	(((bst_vol) - 3500) * 10 / 625)
+#define AW8692X_F0_FARMULA(f0_reg)		(384000 * 10 / (f0_reg))
+#define AW8692X_LRA_FORMULA(lra, d2s_gain)	((6075 * 100 * (lra)) / \
+						(1024 * (d2s_gain)))
+#define AW8692X_VBAT_FORMULA(vbat)		(5 * 1215 * (vbat) / 1024)
+#define AW8692X_RAMADDR_H(base_addr)		((base_addr) >> 8)
+#define AW8692X_RAMADDR_L(base_addr)		((base_addr) & 0x00FF)
+#define AW8692X_BASEADDR_H(base_addr)		((base_addr) >> 8)
+#define AW8692X_BASEADDR_L(base_addr)		((base_addr) & 0x00FF)
+#define AW8692X_FIFO_AE_ADDR_H(base_addr)	((((base_addr) >> 1) >> 4) & 0xF0)
+#define AW8692X_FIFO_AE_ADDR_L(base_addr)	(((base_addr) >> 1) & 0x00ff)
+#define AW8692X_FIFO_AF_ADDR_H(base_addr)	((((base_addr) - ((base_addr) >> 2)) >> 8) & 0x0F)
+#define AW8692X_FIFO_AF_ADDR_L(base_addr)	(((base_addr) - ((base_addr) >> 2)) & 0x00ff)
+
+#define AW_DRV_WIDTH_FARMULA(f0_pre, brk_gain, track_margain) (240000 / \
+				    (f0_pre) - 8 - (brk_gain) - (track_margain))
 
 /*********************************************************
  *
@@ -268,6 +327,19 @@ enum aw_haptic_work_mode {
 	AW_RAM_LOOP_MODE = 5,
 };
 
+enum aw_haptic_irq_status {
+	AW_IRQ_ALMOST_EMPTY = 1,
+	AW_IRQ_ALMOST_FULL = 2,
+	AW_IRQ_BST_SCP = 3,
+	AW_IRQ_BST_OVP = 4,
+	AW_IRQ_UVLO = 5,
+	AW_IRQ_OCD = 6,
+	AW_IRQ_OT = 7,
+	AW_IRQ_LOW_VBAT = 8,
+	AW_IRQ_OV = 9,
+	AW_IRQ_DONE = 10,
+};
+
 enum aw_haptic_bst_mode {
 	AW_BST_BYPASS_MODE = 0,
 	AW_BST_BOOST_MODE = 1,
@@ -311,6 +383,8 @@ enum aw_haptic_cmd {
 	AW_CMD_ENABLE = 1,
 	AW_CMD_HAPTIC = 0x0f,
 	AW_CMD_TP = 0x10,
+	AW_CMD_R_EN = 0x40,
+	AW_CMD_L_EN = 0x80,
 	AW_CMD_SYS = 0xf0,
 	AW_CMD_STOP = 255,
 };
@@ -324,6 +398,19 @@ enum aw_haptic_cali_lra {
 enum aw_haptic_awrw_flag {
 	AW_SEQ_WRITE = 0,
 	AW_SEQ_READ = 1,
+};
+
+/*********************************************************
+ *
+ * Enum aw8671x
+ *
+ *********************************************************/
+enum aw8671x_haptic_chargepump {
+	AW8671X_CHARGEPUMP_CODE0  = 4950,
+	AW8671X_CHARGEPUMP_CODE1  = 5316,
+	AW8671X_CHARGEPUMP_CODE2  = 5661,
+	AW8671X_CHARGEPUMP_CODE3  = 5827,
+	AW8671X_CHARGEPUMP_CODE4  = 6336,
 };
 
 /*********************************************************
@@ -415,11 +502,12 @@ struct aw_haptic_audio {
 struct aw_haptic_dts_info {
 	/* COMMON */
 	uint8_t mode;
-	uint8_t max_bst_vol;
 	uint8_t f0_cali_percent;
-	uint8_t bst_vol_default;
 	uint8_t duration_time[3];
 	uint32_t f0_pre;
+	uint32_t bst_vol_ram;
+	uint32_t bst_vol_rtp;
+	uint32_t bst_vol_default;
 
 	/* AW869X */
 	uint8_t tset;
@@ -441,8 +529,7 @@ struct aw_haptic_dts_info {
 	/* AW869XX AW8671X AW8692X */
 	uint8_t d2s_gain;
 	uint8_t brk_bst_md;
-	uint8_t bst_vol_ram;
-	uint8_t bst_vol_rtp;
+	uint8_t gain_bypass;
 	uint8_t cont_tset;
 	uint8_t cont_drv1_lvl;
 	uint8_t cont_drv2_lvl;
@@ -452,10 +539,10 @@ struct aw_haptic_dts_info {
 	uint8_t cont_brk_gain;
 	uint8_t cont_drv1_time;
 	uint8_t cont_drv2_time;
-	uint8_t cont_drv_width;
 	uint8_t cont_track_margin;
 	uint8_t cont_bst_brk_gain;
 	uint8_t trig_cfg[24];
+	uint32_t cont_lra_vrms;
 	bool is_enabled_auto_bst;
 	bool is_enabled_i2s;
 	bool is_enabled_one_wire;
@@ -480,7 +567,6 @@ struct aw_haptic {
 	uint8_t max_neg_beme;
 	uint8_t activate_mode;
 	uint8_t ram_vbat_comp;
-	uint8_t rtp_routine_on;
 	uint8_t seq[AW_SEQUENCER_SIZE];
 	uint8_t loop[AW_SEQUENCER_SIZE];
 	uint8_t name[15];
@@ -502,7 +588,6 @@ struct aw_haptic {
 	uint32_t lra;
 	uint32_t vbat;
 	uint32_t pre_f0;
-	uint32_t cont_f0;
 	uint32_t rtp_cnt;
 	uint32_t rtp_len;
 	uint32_t gun_type;
@@ -520,9 +605,19 @@ struct aw_haptic {
 	ktime_t kstart;
 	ktime_t kcurrent_time;
 	ktime_t kpre_enter_time;
-
+#ifdef AW_TIKTAP
+	bool tiktap_ready;
+	bool vib_stop_flag;
+	bool tiktap_stop_flag;
+	struct work_struct tiktap_work;
+	struct proc_dir_entry *aw_config_proc;
+	struct mmap_buf_format *start_buf;
+#endif
 	struct device *dev;
 	struct i2c_client *i2c;
+	struct input_dev *input_dev;
+	struct work_struct gain_work;
+	struct work_struct input_vib_work;
 	struct mutex lock;
 	struct hrtimer timer;
 	struct mutex rtp_lock;
@@ -549,6 +644,7 @@ struct aw_haptic_container {
 
 struct aw_haptic_func {
 	int (*get_f0)(struct aw_haptic *);
+	int (*ram_get_f0)(struct aw_haptic *);
 	int (*creat_node)(struct aw_haptic *);
 	int (*get_irq_state)(struct aw_haptic *);
 	int (*check_qualify)(struct aw_haptic *);
@@ -559,7 +655,6 @@ struct aw_haptic_func {
 	void (*get_vbat)(struct aw_haptic *);
 	void (*cont_config)(struct aw_haptic *);
 	void (*offset_cali)(struct aw_haptic *);
-	void (*read_f0)(struct aw_haptic *);
 	void (*play_go)(struct aw_haptic *, bool);
 	void (*ram_init)(struct aw_haptic *, bool);
 	void (*set_bst_peak_cur)(struct aw_haptic *);
@@ -567,7 +662,7 @@ struct aw_haptic_func {
 	void (*set_pwm)(struct aw_haptic *, uint8_t);
 	void (*set_gain)(struct aw_haptic *, uint8_t);
 	void (*play_mode)(struct aw_haptic *, uint8_t);
-	void (*set_bst_vol)(struct aw_haptic *, uint8_t);
+	void (*set_bst_vol)(struct aw_haptic *, uint32_t);
 	void (*set_repeat_seq)(struct aw_haptic *, uint8_t);
 	void (*auto_bst_enable)(struct aw_haptic *, uint8_t);
 	void (*vbat_mode_config)(struct aw_haptic *, uint8_t);
@@ -585,16 +680,17 @@ struct aw_haptic_func {
 	void (*set_rtp_aei)(struct aw_haptic *, bool);
 	void (*upload_lra)(struct aw_haptic *, uint32_t);
 	void (*get_wav_seq)(struct aw_haptic *, uint32_t);
+	void (*get_ram_data)(struct aw_haptic *, char *);
 	void (*bst_mode_config)(struct aw_haptic *, uint8_t);
 	void (*get_first_wave_addr)(struct aw_haptic *, uint8_t *);
 	size_t (*get_wav_loop)(struct aw_haptic *, char *);
-	ssize_t (*get_ram_data)(struct aw_haptic *, char *);
 	ssize_t (*get_reg)(struct aw_haptic *, ssize_t, char *);
 	uint8_t (*get_prctmode)(struct aw_haptic *);
 	uint8_t (*get_trim_lra)(struct aw_haptic *);
 	uint8_t (*get_glb_state)(struct aw_haptic *);
 	uint8_t (*get_osc_status)(struct aw_haptic *);
 	uint8_t (*rtp_get_fifo_afs)(struct aw_haptic *);
+	uint8_t (*rtp_get_fifo_aes)(struct aw_haptic *);
 	uint64_t (*get_theory_time)(struct aw_haptic *);
 
 #ifdef AW_SND_SOC_CODEC
@@ -607,9 +703,6 @@ struct aw_haptic_func {
  * Function Call
  *
  *********************************************************/
-extern char *aw_ram_name;
-extern char aw_rtp_name[][AW_RTP_NAME_MAX];
-
 #ifdef AW869X_DRIVER_ENABLE
 extern struct aw_haptic_func aw869x_func_list;
 #endif
@@ -626,8 +719,8 @@ extern struct aw_haptic_func aw8671x_func_list;
 extern struct aw_haptic_func aw8692x_func_list;
 #endif
 
-extern int i2c_r_bytes(struct aw_haptic *, uint8_t, uint8_t *, uint32_t);
-extern int i2c_w_bytes(struct aw_haptic *, uint8_t, uint8_t *, uint32_t);
-extern int i2c_w_bits(struct aw_haptic *, uint8_t, uint32_t, uint8_t);
+extern int haptic_hv_i2c_reads(struct aw_haptic *, uint8_t, uint8_t *, uint32_t);
+extern int haptic_hv_i2c_writes(struct aw_haptic *, uint8_t, uint8_t *, uint32_t);
+extern int haptic_hv_i2c_write_bits(struct aw_haptic *, uint8_t, uint32_t, uint8_t);
 
 #endif
