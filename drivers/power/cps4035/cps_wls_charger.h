946de07778960f0f861cc88087e316dc8ed21a0d
Jiacheng Ou <oujc1@motorola.com>
2022-04-14 15:03:54 +0800

    wireless charge:factory test process optimize
    wireless charge:
    change wls mos when ldo on
    
    Change-Id: Ia353047eb2594701c4f1737d6215a0432e7bd8f9
    Signed-off-by: Jiacheng Ou <oujc1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2242265
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/cps4035/cps_wls_charger.h
+++ drivers/power/cps4035/cps_wls_charger.h
@@ -263,6 +263,7 @@ struct cps_wls_chrg_chip {
     uint32_t MaxV;
     uint32_t MaxI;
     uint32_t chip_id;
+    bool factory_wls_en;
     //struct work_struct wls_rx_work;
 	wait_queue_head_t  wait_que;
 	bool wls_rx_check_thread_timeout;
