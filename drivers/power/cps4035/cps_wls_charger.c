c35e02f7aaf9f2c801a8f9ca087a26905e547880
Jiacheng Ou <oujc1@motorola.com>
2022-05-07 16:46:37 +0800

    wireless charge:fod thresold to 800
    new fw V1.12 fod thresold need change to
    800
    
    Signed-off-by: Jiacheng Ou <oujc1@motorola.com>
    Change-Id: Ida577a5685f53cec8c86dc808adce520f6d39a75
    Reviewed-on: https://gerrit.mot.com/2262724
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/cps4035/cps_wls_charger.c
+++ drivers/power/cps4035/cps_wls_charger.c
@@ -66,8 +66,8 @@ static bool CPS_RX_CHRG_FULL = false;
 
 static uint32_t fod_i_th_w_folio = 1100;
 static uint32_t fod_ii_th_w_folio = 1100;
-static uint32_t fod_i_th_o_folio = 850;
-static uint32_t fod_ii_th_o_folio = 850;
+static uint32_t fod_i_th_o_folio = 800;
+static uint32_t fod_ii_th_o_folio = 800;
 #ifdef SMART_PEN_SUPPORT
 static bool PEN_MAC0_READY = false;
 static uint32_t cps_pen_mac[6] = {0};
