1b5256662422046915e9b1b0fd5cb8f4dd2b3558
longc103 <longc103@motorola.com>
2022-07-27 11:09:55 +0800

    devonf:fps bringup
    WHY:devonf:2nd fps icnf7318 bringup
    HOW:driver code compatible design
    
    Change-Id: Ibe7a95d84630fa8595f6a7f9d121b657d36f1e14
    Signed-off-by: longc103 <longc103@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2345797
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: liujian <liujian24@lenovo.com>
    Reviewed-by: Wang Wang <wangwang1@lenovo.com>
    Submit-Approved: Jira Key

--- drivers/input/misc/chipone_fps/Android.mk
+++ drivers/input/misc/chipone_fps/Android.mk
@@ -1,6 +1,14 @@
 DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(CHIPONE_GPIO_CTRL_VDD),true)
+    KERNEL_CFLAGS  += CONFIG_CHIPONE_GPIO_CTRL_VDD=y
+endif
+
+ifeq ($(CHIPONE_MTK_KERNEL5XX_WAKE_TYPE), true)
+    KERNEL_CFLAGS  += CONFIG_CHIPONE_MTK_KERNEL5XX_WAKE_TYPE=y
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := fpsensor_mtk_spi.ko
 LOCAL_MODULE_TAGS := optional
