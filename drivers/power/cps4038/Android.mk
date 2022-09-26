b8be5781d2b62bcdd15f8e0e1dbf11af7cb3ebcd
xuwei9 <xuwei9@lenovo.com>
2022-09-26 17:01:45 +0800

    cps4038 original code
    Change-Id: Ib5ecba06531738d33a1570000978c233c0b7febe
    Reviewed-on: https://gerrit.mot.com/2392265
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wei Xu <xuwei9@lenovo.com>
    Reviewed-by: Wang Wang <wangwang1@lenovo.com>
    Submit-Approved: Jira Key

---
+++ drivers/power/cps4038/Android.mk
@@ -0,0 +1,15 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := cps_wls_charger_4038.ko
+LOCAL_MODULE_TAGS := optional
+ifeq ($(DLKM_INSTALL_TO_VENDOR_OUT),true)
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/modules/
+else
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+endif
+#LOCAL_ADDITIONAL_DEPENDENCIES += $(KERNEL_MODULES_OUT)/smart_pen_charger.ko
+#LOCAL_REQUIRED_MODULES := cps4035.ko
+#KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
+include $(DLKM_DIR)/AndroidKernelModule.mk
