d53fd25663b1f8928120cc9ae9c6b59bea7d0787
Jun Weng <wengjun1@motorola.com>
2022-10-25 13:48:22 +0800

    bo-mtk: dlkm: Update touchsceen_mmi driver
    Port from qcom branch including below changes,
    
    (CR): Add a macro to control if cli_gesture created
    (CR): Add CLI touch gesture support on mmi touch screen class
    (CR): Decrease the log of register panel notifier
    (CR): Support double tap to wake up the screen
    (CR): Remove unused variable defination in case build error
    (CR) touchscreen_mmi: active region interface
    (CR) : Fix fod event not working during touch resume
    (CR) : Trigger fod event only when FOD is enabled
    (CR):Support receiving dual-screen notifiactions.
    
    Change-Id: I0afc6678970578a967915d958014fbfdea17baef
    Signed-off-by: Jun Weng <wengjun1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2412673
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wei Deng <dengwei1@motorola.com>
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/touchscreen_mmi/touchscreen_mmi_gesture.c
+++ drivers/input/touchscreen/touchscreen_mmi/touchscreen_mmi_gesture.c
@@ -79,6 +79,7 @@ struct ts_mmi_touch_events_data {
 static struct ts_mmi_sensor_platform_data *sensor_pdata;
 static struct ts_mmi_sensor_platform_data *palm_sensor_pdata;
 static struct ts_mmi_touch_events_data *events_data;
+static struct ts_mmi_sensor_platform_data *cli_sensor_pdata;
 
 #ifdef TS_MMI_TOUCH_GESTURE_LOG_EVENT
 static inline void ts_mmi_touch_log_event(struct touch_event_with_time_data *dst, const struct touch_event_data *src)
@@ -212,6 +213,7 @@ static int ts_mmi_gesture_handler(struct gesture_event_data *gev)
 {
 	int key_code;
 	bool need2report = true;
+	struct ts_mmi_dev *touch_cdev = sensor_pdata->touch_cdev;
 
 	switch (gev->evcode) {
 	case 1:
@@ -220,10 +222,22 @@ static int ts_mmi_gesture_handler(struct gesture_event_data *gev)
 			break;
 	case 2:
 		key_code = KEY_F2;
+		if(gev->evdata.x == 0)
+			gev->evdata.x = touch_cdev->pdata.fod_x ;
+		if(gev->evdata.y== 0)
+			gev->evdata.y = touch_cdev->pdata.fod_y;
 		input_report_abs(sensor_pdata->input_sensor_dev, ABS_X, gev->evdata.x);
 		input_report_abs(sensor_pdata->input_sensor_dev, ABS_Y, gev->evdata.y);
 		pr_info("%s: zero tap; x=%x, y=%x\n", __func__, gev->evdata.x, gev->evdata.y);
-			break;
+		break;
+	case 3:
+		key_code = KEY_F3;
+		pr_info("%s: zero tap up\n", __func__);
+		break;
+	case 4:
+		key_code = KEY_F4;
+		pr_info("%s: double tap\n", __func__);
+		break;
 	default:
 		need2report = false;
 		pr_info("%s: unknown id=%x\n", __func__, gev->evcode);
@@ -317,7 +331,7 @@ static int ts_mmi_touch_event_edge_handler(struct touch_event_data *tev,  struct
 static int ts_mmi_touch_event_handler(struct touch_event_data *tev,  struct input_dev *input_dev)
 {
 	struct ts_mmi_dev *touch_cdev = events_data->touch_cdev;
-	int ret = 0;
+	__maybe_unused int ret = 0;
 
 #ifdef TS_MMI_TOUCH_EDGE_GESTURE
 	ts_mmi_touch_event_edge_handler(tev, input_dev);
@@ -370,6 +384,8 @@ bool ts_mmi_is_sensor_enable(void)
 static int ts_mmi_sensor_set_enable(struct sensors_classdev *sensors_cdev,
 		unsigned int enable)
 {
+#if !defined(CONFIG_BOARD_USES_DOUBLE_TAP_CTRL) && \
+	!defined(CONFIG_BOARD_USES_CLI_DOUBLE_TAP_CTRL)
 	struct ts_mmi_sensor_platform_data *sensor_pdata = container_of(
 			sensors_cdev, struct ts_mmi_sensor_platform_data, ps_cdev);
 	struct ts_mmi_dev *touch_cdev = sensor_pdata->touch_cdev;
@@ -382,6 +398,7 @@ static int ts_mmi_sensor_set_enable(struct sensors_classdev *sensors_cdev,
 	} else {
 		dev_err(DEV_TS, "%s: unknown enable symbol\n", __func__);
 	}
+#endif
 	return 0;
 }
 
@@ -406,6 +423,26 @@ static int ts_mmi_palm_set_enable(struct sensors_classdev *sensors_cdev,
 
 static struct sensors_classdev __maybe_unused sensors_touch_cdev = {
 	.name = "dt-gesture",
+	.vendor = "Motorola",
+	.version = 1,
+	.type = SENSOR_TYPE_MOTO_DOUBLE_TAP,
+	.max_range = "5.0",
+	.resolution = "5.0",
+	.sensor_power = "1",
+	.min_delay = 0,
+	.max_delay = 0,
+	/* WAKE_UP & SPECIAL_REPORT */
+	.flags = 1 | 6,
+	.fifo_reserved_event_count = 0,
+	.fifo_max_event_count = 0,
+	.enabled = 0,
+	.delay_msec = 200,
+	.sensors_enable = NULL,
+	.sensors_poll_delay = NULL,
+};
+
+static struct sensors_classdev __maybe_unused cli_sensors_touch_cdev = {
+	.name = "s-dt-gesture",
 	.vendor = "Motorola",
 	.version = 1,
 	.type = SENSOR_TYPE_MOTO_DOUBLE_TAP,
@@ -483,6 +520,8 @@ int ts_mmi_gesture_init(struct ts_mmi_dev *touch_cdev)
 	__set_bit(EV_KEY, sensor_input_dev->evbit);
 	__set_bit(KEY_F1, sensor_input_dev->keybit);
 	__set_bit(KEY_F2, sensor_input_dev->keybit);
+	__set_bit(KEY_F3, sensor_input_dev->keybit);
+	__set_bit(KEY_F4, sensor_input_dev->keybit);
 	__set_bit(EV_ABS, sensor_input_dev->evbit);
 	__set_bit(EV_SYN, sensor_input_dev->evbit);
 	/* TODO: fill in real screen resolution */
@@ -538,6 +577,81 @@ int ts_mmi_gesture_remove(struct ts_mmi_dev *touch_cdev)
 	devm_kfree(DEV_TS, events_data);
 	sensor_pdata = NULL;
 	events_data = NULL;
+
+	return 0;
+}
+
+int ts_mmi_cli_gesture_init(struct ts_mmi_dev *touch_cdev)
+{
+	struct input_dev *sensor_input_dev;
+	int err;
+
+	sensor_input_dev = input_allocate_device();
+	if (!sensor_input_dev) {
+		dev_err(DEV_TS, "%s: Failed to allocate input device", __func__);
+		goto exit;
+	}
+
+	cli_sensor_pdata = devm_kzalloc(&sensor_input_dev->dev,
+			sizeof(struct ts_mmi_sensor_platform_data), GFP_KERNEL);
+	if (!cli_sensor_pdata) {
+		dev_err(DEV_TS, "%s: Failed to allocate memory", __func__);
+		goto free_sensor_input_dev;
+	}
+
+	__set_bit(EV_KEY, sensor_input_dev->evbit);
+	__set_bit(KEY_F1, sensor_input_dev->keybit);
+	__set_bit(KEY_F2, sensor_input_dev->keybit);
+	__set_bit(KEY_F3, sensor_input_dev->keybit);
+	__set_bit(KEY_F4, sensor_input_dev->keybit);
+	__set_bit(EV_ABS, sensor_input_dev->evbit);
+	__set_bit(EV_SYN, sensor_input_dev->evbit);
+	/* TODO: fill in real screen resolution */
+	input_set_abs_params(sensor_input_dev, ABS_X, 0, 4096, 0, 0);
+	input_set_abs_params(sensor_input_dev, ABS_Y, 0, 4096, 0, 0);
+
+	sensor_input_dev->name = "s-double-tap";
+	cli_sensor_pdata->input_sensor_dev = sensor_input_dev;
+
+	err = input_register_device(sensor_input_dev);
+	if (err) {
+		dev_err(DEV_TS, "%s: Unable to register device, err=%d", __func__, err);
+		goto free_sensor_pdata;
+	}
+
+	cli_sensor_pdata->ps_cdev = cli_sensors_touch_cdev;
+	cli_sensor_pdata->ps_cdev.sensors_enable = ts_mmi_sensor_set_enable;
+	cli_sensor_pdata->touch_cdev = touch_cdev;
+
+	err = sensors_classdev_register(&sensor_input_dev->dev,
+				&cli_sensor_pdata->ps_cdev);
+	if (err)
+		goto unregister_sensor_input_device;
+
+	/* export report gesture function to vendor */
+	touch_cdev->mdata->exports.report_gesture = ts_mmi_gesture_handler;
+
+	return 0;
+
+unregister_sensor_input_device:
+	input_unregister_device(sensor_input_dev);
+	sensor_input_dev = NULL;
+free_sensor_pdata:
+	if (sensor_input_dev && cli_sensor_pdata)
+		devm_kfree(&sensor_input_dev->dev, cli_sensor_pdata);
+free_sensor_input_dev:
+	if (sensor_input_dev)
+		input_free_device(sensor_input_dev);
+exit:
+	return 1;
+}
+
+int ts_mmi_cli_gesture_remove(struct ts_mmi_dev *touch_cdev)
+{
+	sensors_classdev_unregister(&cli_sensor_pdata->ps_cdev);
+	input_unregister_device(cli_sensor_pdata->input_sensor_dev);
+	devm_kfree(&cli_sensor_pdata->input_sensor_dev->dev, cli_sensor_pdata);
+	cli_sensor_pdata = NULL;
 
 	return 0;
 }
