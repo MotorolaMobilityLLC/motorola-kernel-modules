f6a7958a6bab5772f12dd01c7711a4a1bf8cdbba
Lei Chen <chenlei18@lenovo.com>
2022-11-25 11:36:13 +0800

    bq2597x:use the cell voltage from sc8541
    Changes,
    Use the cell volatage from sc8541 and enable adc when charger
    plug in.
    
    Change-Id: I89feef1ee9b817283aead3ec3c672dd2f29a9b59
    Signed-off-by: Lei Chen <chenlei18@mt.com>
    Reviewed-on: https://gerrit.mot.com/2439974
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/mmi_parallel_charger/mmi_charger_class.h
+++ drivers/power/mmi_parallel_charger/mmi_charger_class.h
@@ -101,6 +101,7 @@ struct mmi_charger_ops {
 	int (*update_charger_status)(struct mmi_charger_device *chrg);
 	int (*update_charger_error)(struct mmi_charger_device *chrg);
 	int (*clear_charger_error)(struct mmi_charger_device *chrg);
+	int (*enable_adc)(struct mmi_charger_device *chrg, bool en);
 };
 
 
@@ -121,6 +122,7 @@ extern int mmi_set_input_current(struct mmi_charger_device *chrg, u32 uA);
 extern int mmi_update_charger_status(struct mmi_charger_device *chrg);
 extern int mmi_update_charger_error(struct mmi_charger_device *chrg);
 extern int mmi_clear_charger_error(struct mmi_charger_device *chrg);
+extern int mmi_enable_adc(struct mmi_charger_device *chrg, bool en);
 extern struct mmi_charger_device *mmi_charger_device_register(const char *name,
 		const char *psy_name,struct device *parent, void *devdata,
 		const struct mmi_charger_ops *ops);
