e704992042486122689b9ffbdb15233641c2769a
wingte72 <wingte72@motorola.com>
2022-12-01 19:34:09 +0800

    ili9882_mmi: register pen_detect after FW loaded
    ili9882_mmi:
    register pen_detect after FW successfully loaded
    to fix potential TP invalid issue
    
    Change-Id: I475fb8b454420e3254b407dc964f4f3a827bf7b2
    Reviewed-on: https://gerrit.mot.com/2448182
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/ili9882_mmi/ili9882_qcom.c
+++ drivers/input/touchscreen/ili9882_mmi/ili9882_qcom.c
@@ -25,9 +25,6 @@
 #define DTS_INT_GPIO	"touch,irq-gpio"
 #define DTS_RESET_GPIO	"touch,reset-gpio"
 #define DTS_OF_NAME	"tchip,ilitek"
-#ifdef ILITEK_PEN_NOTIFIER
-int ilitek_pen_detect_set(u8 pen_detect);
-#endif
 
 enum touch_state {
 	TOUCH_DEEP_SLEEP_STATE = 0,
@@ -834,40 +831,6 @@ void ilitek_plat_charger_init(void)
 #endif
 #endif
 
-#ifdef ILITEK_PEN_NOTIFIER
-#define ENABLE_PASSIVE_PEN_MODE_CMD 0x01
-#define DISABLE_PASSIVE_PEN_MODE_CMD 0x00
-int ilitek_pen_detect_set(u8 pen_detect) {
-        int ret = 0;
-
-        ILI_DBG("enter\n");
-        if (PEN_DETECTION_PULL == pen_detect)
-                ret = ili_ic_func_ctrl("passive_pen", ENABLE_PASSIVE_PEN_MODE_CMD);
-        else
-                ret = ili_ic_func_ctrl("passive_pen", DISABLE_PASSIVE_PEN_MODE_CMD);
-
-        return ret;
-}
-
-static int pen_notifier_callback(struct notifier_block *self,
-                                unsigned long event, void *data)
-{
-        ILI_INFO("Received event(%lu) for pen detection\n", event);
-
-        mutex_lock(&ilits->touch_mutex);
-        if (event == PEN_DETECTION_PULL)
-                ilits->pen_detect_flag = PEN_DETECTION_PULL;
-        else
-                ilits->pen_detect_flag = PEN_DETECTION_INSERT;
-
-        ILI_DBG("invoke ilitek_pen_detect_set start");
-        ilitek_pen_detect_set(ilits->pen_detect_flag);
-        mutex_unlock(&ilits->touch_mutex);
-
-        return 0;
-}
-#endif
-
 static int ilitek_plat_probe(void)
 {
 	ILI_INFO("platform probe\n");
@@ -896,11 +859,6 @@ static int ilitek_plat_probe(void)
 	ilits->pm_suspend = false;
 	init_completion(&ilits->pm_completion);
 
-#ifdef ILITEK_PEN_NOTIFIER
-	ilits->pen_detect_flag = PEN_DETECTION_INSERT;
-        ilits->pen_notif.notifier_call = pen_notifier_callback;
-        pen_detection_register_client(&ilits->pen_notif);
-#endif
 #if CHARGER_NOTIFIER_CALLBACK
 #if KERNEL_VERSION(4, 1, 0) <= LINUX_VERSION_CODE
 	/* add_for_charger_start */
