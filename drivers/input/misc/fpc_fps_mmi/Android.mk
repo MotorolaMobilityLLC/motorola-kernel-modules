c33a3d0340326df8cc1bdc8daa6ca35ee4844698
liujian24 <liujian24@lenovo.com>
2022-12-15 17:02:55 +0800

    Gnevan:Improve FPC1553 unlock speed
    Need to improve FPS unlock performance for FPC1553
    Bind TA with big core when doing FPS unlock
    
    Change-Id: I383283abcbb685f77fb540b0ff2654495fa97603
    Reviewed-on: https://gerrit.mot.com/2462399
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: liujian <liujian24@lenovo.com>
    Reviewed-by: Wang Wang <wangwang1@lenovo.com>
    Submit-Approved: Jira Key

--- drivers/input/misc/fpc_fps_mmi/Android.mk
+++ drivers/input/misc/fpc_fps_mmi/Android.mk
@@ -5,6 +5,10 @@ ifeq ($(BOARD_HAS_MULTI_FPS),true)
 	KERNEL_CFLAGS += CONFIG_INPUT_MISC_FPC1020_SAVE_TO_CLASS_DEVICE=y
 endif
 
+ifeq ($(FPC_TEE_BOOST),true)
+        KERNEL_CFLAGS += FPC_TEE_BOOST=y
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := fpc_mtk_tee.ko
 LOCAL_MODULE_TAGS := optional
