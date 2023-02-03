e17b572511ceea38da00c2b166f2cc79743d7f36
liujian24 <liujian24@lenovo.com>
2023-02-03 15:18:37 +0800

    Gnevan:Disable screen event
    Egist FPS driver generate screen event and this trigger
    unexpected FPS interrupt, need disable above event.
    
    Change-Id: I4628f541b66daccb16147cc494002fed79a84b17
    Reviewed-on: https://gerrit.mot.com/2501606
    Reviewed-by: liujian <liujian24@lenovo.com>
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Wang Wang <wangwang1@lenovo.com>
    Submit-Approved: Jira Key

--- drivers/input/misc/ets_bix_mmi/Android.mk
+++ drivers/input/misc/ets_bix_mmi/Android.mk
@@ -1,6 +1,10 @@
 DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(EGIS_SCREEN_EVENT_DISABLE),true)
+     KERNEL_CFLAGS += CONFIG_EGIS_SCREEN_EVENT_DISABLE=y
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := ets_bix_mmi.ko
 LOCAL_MODULE_TAGS := optional
