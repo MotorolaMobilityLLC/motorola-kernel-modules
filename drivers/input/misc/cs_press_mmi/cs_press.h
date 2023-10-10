4b9f41e6b58348dbe7f901c46a1d6d8c78783937
Wenge Zhu <zhuwg1@motorola.com>
2023-10-10 17:18:39 +0800

    Fix CHIPSEA driver source code license conflict
    Confirmed with vendor, these codes could be published under the terms
    of the GNU General Public License,so remove vendor's private and
    confidential declarations.
    
    Change-Id: I49f6ba6ffc7fdde3914f3b4d277a0879354cb654
    Signed-off-by: Wenge Zhu <zhuwg1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2762975
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yuting Chen <chenyt17@motorola.com>
    Reviewed-by: Jichao Zou <zoujc@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/misc/cs_press_mmi/cs_press.h
+++ drivers/input/misc/cs_press_mmi/cs_press.h
@@ -1,20 +1,19 @@
 /*
- *-----------------------------------------------------------------------------
- * The confidential and proprietary information contained in this file may
- * only be used by a person authorised under and to the extent permitted
- * by a subsisting licensing agreement from  CHIPSEA.
+ * Driver for CHIPSEA forcetouch sensor
+ * Copyright (C) 2023 CHIPSEA
  *
- *            (C) COPYRIGHT 2020 SHENZHEN CHIPSEA TECHNOLOGIES CO.,LTD.
- *                ALL RIGHTS RESERVED
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This entire notice must be reproduced on all copies of this file
- * and copies of this file may only be made by a person if such person is
- * permitted to do so under the terms of a subsisting license agreement
- * from CHIPSEA.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *      Release Information : CSA37F71 chip forcetouch fw driver head file
- *      version : v0.2
- *-----------------------------------------------------------------------------
+ * Release Information : CSA37F71 chip forcetouch fw driver head file
+ * version : v0.2
  */
 
 #ifndef _CS_PRESS_H_
