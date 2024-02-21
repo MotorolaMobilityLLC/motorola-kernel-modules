aed62404b952afb30c4088c04125af56419817c1
Xunjie Xu <xuxj12@motorola.com>
2024-02-21 18:48:01 +0800

    charger: remove reset charger pump func
    Write glink info at shutdown point may failed for glink already shutdown,
    and write operation will timeout caused shutdown thread take too long.
    Remove this operation and recommand to add chargerpump watchdog
    in adsp side.
    
    Change-Id: Ia07a96bbc40cd0e2f9b3e7f6c3dfb1098afba3a7
    Signed-off-by: Xunjie Xu <xuxj12@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2885350
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Wentao Xu <xuwt2@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit b46092837d9d9c4b8db62ebfd4f25ded549baeaf)

--- drivers/power/qti_glink_charger/qti_glink_charger.c
+++ drivers/power/qti_glink_charger/qti_glink_charger.c
@@ -3410,31 +3410,11 @@ static int qti_charger_init(struct qti_charger *chg)
 	return 0;
 }
 
-bool qti_charger_reset_chargepump(struct qti_charger *chg)
-{
-	unsigned long reset = 0x1;
-	int rc ;
-
-	if(!chg) {
-		pr_err("QTI: chip not valid");
-		return false;
-	}
-
-	rc = qti_charger_write(chg, OEM_PROP_MASTER_SWITCHEDCAP_RESET, &reset, sizeof(reset));
-	if(rc) {
-		mmi_err(chg, "qit charger reset charger pump register fail:%d\n", rc);
-	}
-
-	return true;
-}
-
 static void qti_charger_shutdown(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	struct qti_charger *chg= dev_get_drvdata(dev);
 
-	//reset charger pump register before shutdown.
-	qti_charger_reset_chargepump(chg);
 	mmi_info(chg, "qti_charger_shutdown\n");
 
 	return;
