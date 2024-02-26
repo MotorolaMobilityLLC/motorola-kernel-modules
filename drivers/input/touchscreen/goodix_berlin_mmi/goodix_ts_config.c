0cf86372bab5be0eac676dd028473a0747e6b353
hujing3 <hujing3@lenovo.com>
2024-02-26 14:58:56 +0800

    goodix_berlin malmo game mode support
    goodix_berlin_mmi:
    malmo game mode support
    default touch report rate is 240Hz and high report rate is 360Hz.
    
    Change-Id: I16b5953b444139cff0c17ed79cf119cce1de8c06
    Signed-off-by: hujing3 <hujing3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2888971
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <lvxj4@motorola.com>
    Reviewed-by: Wei Deng <dengwei1@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 75e39c7cac6be12e5aea44d4db3ff12f87e24fff)

--- drivers/input/touchscreen/goodix_berlin_mmi/goodix_ts_config.c
+++ drivers/input/touchscreen/goodix_berlin_mmi/goodix_ts_config.c
@@ -17,7 +17,8 @@ struct goodix_ic_report_rate_config report_rate_config_info = {
 	||defined(PRODUCT_EQS) || defined(PRODUCT_RTWO) || defined(PRODUCT_BRONCO) \
 	|| defined(PRODUCT_ZEEKR) || defined(PRODUCT_LYNKCO) || defined(PRODUCT_BANGKK) \
 	|| defined(PRODUCT_EQE) || defined(PRODUCT_CTWO) || defined(PRODUCT_BOSTON) \
-	|| defined(PRODUCT_AVATRN) || defined(PRODUCT_CUSCO) || defined(PRODUCT_ARCFOX)
+	|| defined(PRODUCT_AVATRN) || defined(PRODUCT_CUSCO) || defined(PRODUCT_ARCFOX) \
+	|| defined(PRODUCT_MALMO)
 	.rate_config_count = 3,
 	.refresh_rate_ctrl = 0,
 	.interpolation_ctrl = 1,
