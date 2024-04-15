f17c72a35ce3fdf689f3041500b6f73a0fb93aec
huangzq2 <huangzq2@motorola.com>
2024-04-15 17:48:24 +0800

    optimize kernel mm
    To reduce the workload and lock contention of kswapd and direct reclaim,
    implement async lru shrinker.
    
    Change-Id: I0db01a2eee64c104854e6031de7e947c4ced6780
    Signed-off-by: huangzq2 <huangzq2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2937646
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/moto_mm/mm_main.c
+++ drivers/moto_mm/mm_main.c
@@ -21,8 +21,8 @@
 
 #include "mm_common.h"
 
-bool mem_alloc_hook_registered = false;
-
+// 5.4 - 5.15 tune_inactive_ratio
+#if defined(TUNE_INACTIVE_SUPPORTED)
 static void tune_inactive_ratio_hook(void *data, unsigned long *inactive_ratio, int file)
 {
 	if (file)
@@ -32,88 +32,72 @@ static void tune_inactive_ratio_hook(void *data, unsigned long *inactive_ratio, 
 
 	return;
 }
-
-//5.15+
-static void alloc_pages_slowpath_hook(void *data, gfp_t gfp_mask, unsigned int order, unsigned long alloc_start)
-{
-	u64 wait_ms = jiffies_to_msecs(jiffies - alloc_start);
-	if (moto_alloc_warn_ms > 0 && wait_ms > moto_alloc_warn_ms){
-		int fg = task_in_top_app_group(current);
-		mm_debug("alloc, %s, %d:%d, order %d, wait %lld ms!\n", (fg? "fg":"bg"), current->tgid, current->pid, order, wait_ms);
-	}
-}
-
-//5.10
-static void alloc_pages_slowpath_begin_hook(void *data, gfp_t gfp_mask, unsigned int order, unsigned long *alloc_start)
-{
-	*alloc_start = jiffies;
-}
-
-static void alloc_pages_slowpath_end_hook(void *data, gfp_t gfp_mask, unsigned int order, unsigned long alloc_start)
-{
-	alloc_pages_slowpath_hook(data, gfp_mask, order, alloc_start);
-}
-
-int register_mem_alloc_hooks(void)
-{
-	int rc;
-
-    if (!mem_alloc_hook_registered) {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
-		REGISTER_HOOK(alloc_pages_slowpath);
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
-        REGISTER_HOOK(alloc_pages_slowpath_begin);
-		REGISTER_HOOK(alloc_pages_slowpath_end);
 #endif
-		mem_alloc_hook_registered = true;
-	}
-	return 0;
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
-ERROR_OUT(alloc_pages_slowpath):
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
-ERROR_OUT(alloc_pages_slowpath_begin):
-ERROR_OUT(alloc_pages_slowpath_end):
-#endif
-	return rc;
-}
-
-void unregister_mem_alloc_hooks(void)
+// 5.10 - 5.15 skip drain all pages to speed up direct reclaim
+#if defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED)
+static void drain_all_pages_bypass_hook(void *data, gfp_t gfp_mask, unsigned int order, unsigned long alloc_flags,
+					int migratetype, unsigned long did_some_progress, bool *bypass)
 {
-	if (mem_alloc_hook_registered) {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
-		UNREGISTER_HOOK(alloc_pages_slowpath);
-#else
-        UNREGISTER_HOOK(alloc_pages_slowpath_begin);
-		UNREGISTER_HOOK(alloc_pages_slowpath_end);
-#endif
-		mem_alloc_hook_registered = false;
-	}
+	*bypass = true;
 }
+#endif
+
+// 5.10 - 5.15 skip pages mapcount >= 20
+#if defined(MAPPED_PROTECTOR_SUPPORTED)
+static void page_referenced_check_bypass_hook(void *data, struct page *page, unsigned long nr_to_scan, int lru, bool *bypass)
+{
+	if ((atomic_read(&page->_mapcount) + 1) >= 20)
+		*bypass = true;
+}
+#endif
 
 static int register_all_hooks(void)
 {
+#if defined(TUNE_INACTIVE_SUPPORTED) || defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED) || defined(MAPPED_PROTECTOR_SUPPORTED)
 	int rc;
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 0)
-	/* tune_inactive_ratio_hook */
+#if defined(TUNE_INACTIVE_SUPPORTED)
 	REGISTER_HOOK(tune_inactive_ratio);
-#endif
+#endif // defined(TUNE_INACTIVE_SUPPORTED)
+#if defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED)
+	REGISTER_HOOK(drain_all_pages_bypass);
+#endif // defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED)
+#if defined(MAPPED_PROTECTOR_SUPPORTED)
+	REGISTER_HOOK(page_referenced_check_bypass);
+#endif // defined(MAPPED_PROTECTOR_SUPPORTED)
 	return 0;
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 0)
+#if defined(MAPPED_PROTECTOR_SUPPORTED)
+	UNREGISTER_HOOK(page_referenced_check_bypass);
+ERROR_OUT(page_referenced_check_bypass):
+#endif // defined(MAPPED_PROTECTOR_SUPPORTED)
+#if defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED)
+	UNREGISTER_HOOK(drain_all_pages_bypass);
+ERROR_OUT(drain_all_pages_bypass):
+#endif // defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED)
+#if defined(TUNE_INACTIVE_SUPPORTED)
 	UNREGISTER_HOOK(tune_inactive_ratio);
 ERROR_OUT(tune_inactive_ratio):
-#endif
+#endif // defined(TUNE_INACTIVE_SUPPORTED)
 	return rc;
+
+#else
+	return 0;
+#endif // defined(TUNE_INACTIVE_SUPPORTED) || defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED) || defined(MAPPED_PROTECTOR_SUPPORTED)
 }
 
 static void unregister_all_hooks(void)
 {
-#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 0)
+#if defined(MAPPED_PROTECTOR_SUPPORTED)
+	UNREGISTER_HOOK(page_referenced_check_bypass);
+#endif // defined(MAPPED_PROTECTOR_SUPPORTED)
+#if defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED)
+	UNREGISTER_HOOK(drain_all_pages_bypass);
+#endif // defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED)
+#if defined(TUNE_INACTIVE_SUPPORTED)
 	UNREGISTER_HOOK(tune_inactive_ratio);
-#endif
-	unregister_mem_alloc_hooks();
+#endif // defined(TUNE_INACTIVE_SUPPORTED)
 }
 
 static int __init moto_mm_init(void)
@@ -125,9 +109,18 @@ static int __init moto_mm_init(void)
 		return ret;
 
 	ret = register_all_hooks();
-	if (ret != 0) {
+	if (ret != 0)
 		return ret;
-	}
+
+#if defined(MM_INFO_SUPPORTED)
+	if (moto_mm_info_enabled > 0)
+		mm_info_init();
+#endif // defined(MM_INFO_SUPPORTED)
+
+#if defined(LRU_SHRINKER_SUPPORTED)
+	if (moto_lru_shrinker_enabled > 0)
+		mm_lru_shrinker_init();
+#endif // defined(LRU_SHRINKER_SUPPORTED)
 
 	pr_info("moto_mm_init succeed!\n");
 	return 0;
@@ -135,6 +128,14 @@ static int __init moto_mm_init(void)
 
 static void __exit moto_mm_exit(void)
 {
+#if defined(LRU_SHRINKER_SUPPORTED)
+	mm_lru_shrinker_exit();
+#endif // defined(LRU_SHRINKER_SUPPORTED)
+
+#if defined(MM_INFO_SUPPORTED)
+	mm_info_exit();
+#endif // defined(MM_INFO_SUPPORTED)
+
 	unregister_all_hooks();
 	moto_mm_proc_deinit();
 
