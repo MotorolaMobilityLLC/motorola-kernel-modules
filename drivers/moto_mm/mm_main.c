877d2e6df4d20cb9a1992e0091b216866d52f552
raolan1 <raolan1@lenovo.com>
2024-04-15 15:41:21 +0800

    Add alloc_wait function
    1.Add alloc_pages_slowpath_hook to get alloc wait time
    2.Add moto_alloc_warn_ms to measure alloc wait time
    
    Change-Id: I4eb0ef1f4fcf44ecd24e6c6266c199315027b8a6
    Signed-off-by: raolan1 <raolan1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2937508
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/moto_mm/mm_main.c
+++ drivers/moto_mm/mm_main.c
@@ -21,6 +21,8 @@
 
 #include "mm_common.h"
 
+bool mem_alloc_hook_registered = false;
+
 static void tune_inactive_ratio_hook(void *data, unsigned long *inactive_ratio, int file)
 {
 	if (file)
@@ -29,6 +31,64 @@ static void tune_inactive_ratio_hook(void *data, unsigned long *inactive_ratio, 
 		*inactive_ratio = 1;
 
 	return;
+}
+
+//5.15+
+static void alloc_pages_slowpath_hook(void *data, gfp_t gfp_mask, unsigned int order, unsigned long alloc_start)
+{
+	u64 wait_ms = jiffies_to_msecs(jiffies - alloc_start);
+	if (moto_alloc_warn_ms > 0 && wait_ms > moto_alloc_warn_ms){
+		int fg = task_in_top_app_group(current);
+		mm_debug("alloc, %s, %d:%d, order %d, wait %lld ms!\n", (fg? "fg":"bg"), current->tgid, current->pid, order, wait_ms);
+	}
+}
+
+//5.10
+static void alloc_pages_slowpath_begin_hook(void *data, gfp_t gfp_mask, unsigned int order, unsigned long *alloc_start)
+{
+	*alloc_start = jiffies;
+}
+
+static void alloc_pages_slowpath_end_hook(void *data, gfp_t gfp_mask, unsigned int order, unsigned long alloc_start)
+{
+	alloc_pages_slowpath_hook(data, gfp_mask, order, alloc_start);
+}
+
+int register_mem_alloc_hooks(void)
+{
+	int rc;
+
+    if (!mem_alloc_hook_registered) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
+		REGISTER_HOOK(alloc_pages_slowpath);
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
+        REGISTER_HOOK(alloc_pages_slowpath_begin);
+		REGISTER_HOOK(alloc_pages_slowpath_end);
+#endif
+		mem_alloc_hook_registered = true;
+	}
+	return 0;
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
+ERROR_OUT(alloc_pages_slowpath):
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
+ERROR_OUT(alloc_pages_slowpath_begin):
+ERROR_OUT(alloc_pages_slowpath_end):
+#endif
+	return rc;
+}
+
+void unregister_mem_alloc_hooks(void)
+{
+	if (mem_alloc_hook_registered) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
+		UNREGISTER_HOOK(alloc_pages_slowpath);
+#else
+        UNREGISTER_HOOK(alloc_pages_slowpath_begin);
+		UNREGISTER_HOOK(alloc_pages_slowpath_end);
+#endif
+		mem_alloc_hook_registered = false;
+	}
 }
 
 static int register_all_hooks(void)
@@ -53,6 +113,7 @@ static void unregister_all_hooks(void)
 #if LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 0)
 	UNREGISTER_HOOK(tune_inactive_ratio);
 #endif
+	unregister_mem_alloc_hooks();
 }
 
 static int __init moto_mm_init(void)
