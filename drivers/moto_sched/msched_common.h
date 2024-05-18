b051b61e2b29050d0ec979a79eb6bfc4b73010fe
huangzq2 <huangzq2@motorola.com>
2024-05-18 18:12:28 +0800

    support moto_sched on mtk 5.15
    Change-Id: I5fcb6f02d422e32e12d14fa5154601f0e741382e
    Signed-off-by: huangzq2 <huangzq2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2987113
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/3342098
    Reviewed-by: Ravindranath Sattaru <sravindr@motorola.com>
    (cherry picked from commit 44cf6df40192ae42ac59920b68263fb54bcaa0a1)

--- drivers/moto_sched/msched_common.h
+++ drivers/moto_sched/msched_common.h
@@ -25,12 +25,6 @@
 #include <linux/sched/walt.h>
 #endif
 
-#if IS_ENABLED(CONFIG_MTK_SCHED_VIP_TASK)
-#include <linux/sched/cputime.h>
-#include <kernel/sched/sched.h>
-#include <drivers/misc/mediatek/sched/common.h>
-#endif
-
 #define VERION 1010
 
 #define cond_trace_printk(cond, fmt, ...)	\
