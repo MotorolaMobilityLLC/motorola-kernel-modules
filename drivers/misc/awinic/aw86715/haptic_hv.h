36039105accb333397ac0533f4451924bf867610
wuwl10 <wuwl10@lenovo.com>
2024-06-24 11:31:47 +0800

    aw86715: disable AW_INPUT_FRAMEWORK
    Disable the define of AW_INPUT_FRAMEWORK
    It will make keyboard-> Redirect vibration visible and unit
    can not vibrate with the  menu enable.
    
    Change-Id: I62b87d04cd5f561e965e2f3a688f434a960dd3aa
    Signed-off-by: wuwl10 <wuwl10@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3008853
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Wanlong Zhou <zhouwl@motorola.com>
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/awinic/aw86715/haptic_hv.h
+++ drivers/misc/awinic/aw86715/haptic_hv.h
@@ -82,7 +82,7 @@
 #define AW_CHECK_RAM_DATA
 #define AW_READ_BIN_FLEXBALLY
 #define AW_LRA_F0_DEFAULT
-#define AW_INPUT_FRAMEWORK
+/* #define AW_INPUT_FRAMEWORK */
 /* #define AW_TIKTAP */
 /* #define AW_DOUBLE */
 /* #define AW_DURATION_DECIDE_WAVEFORM */
