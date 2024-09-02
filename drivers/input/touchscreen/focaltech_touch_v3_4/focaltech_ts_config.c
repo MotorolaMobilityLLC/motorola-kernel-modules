7fd82741dbc05195612c35a43e8114b73173cfaf
hujing3 <hujing3@lenovo.com>
2024-09-02 14:40:02 +0800

    focaltech_touch_v3_4: game mode support improve
    focaltech_touch_v3_4:
    add game mode config to support game report rate
    
    Change-Id: Iede89ae22897d90e85de95751b5644c4cbceb56d
    Signed-off-by: hujing3 <hujing3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3075008
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wei Deng <dengwei1@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/focaltech_touch_v3_4/focaltech_ts_config.c
+++ drivers/input/touchscreen/focaltech_touch_v3_4/focaltech_ts_config.c
@@ -13,7 +13,7 @@
 #include "focaltech_ts_config.h"
 
 struct focaltech_ic_report_rate_config report_rate_config_info = {
-#if defined(PRODUCT_BANGKK)|| defined(PRODUCT_AVATRN)
+#if defined(CONFIG_FTS_GAME_MODE_EN) || defined(PRODUCT_BANGKK)|| defined(PRODUCT_AVATRN)
 	.rate_config_count = 2,
 	.refresh_rate_ctrl = 0,
 	.interpolation_ctrl = 1,
