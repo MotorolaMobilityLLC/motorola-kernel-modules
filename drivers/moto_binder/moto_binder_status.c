0a9bd1564e0af3f695496d06d6787a143ab9885b
huangzq2 <huangzq2@motorola.com>
2024-09-10 05:52:48 +0800

    moto_binder: support binder_check on uid
    Change-Id: Id0b2698ce523a80f8c8e22daa2fff85401256c74
    Signed-off-by: huangzq2 <huangzq2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3081820
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit f468342354c9ac15e3b347000861d02dcada5b46)
    Reviewed-on: https://gerrit.mot.com/3128253

--- drivers/moto_binder/moto_binder_status.c
+++ drivers/moto_binder/moto_binder_status.c
@@ -25,19 +25,23 @@
 
 #define ASYNC_REPORT_DISABLE 1
 
+
+struct hlist_head *binder_procs = NULL;
+struct mutex *binder_procs_lock = NULL;
+
 /**
  * binder_inner_proc_lock() - Acquire inner lock for given binder_proc
  * @proc: struct binder_proc to acquire
  *
  * Acquires proc->inner_lock. Used to protect todo lists
  */
-// #define binder_inner_proc_lock(proc) _binder_inner_proc_lock(proc, __LINE__)
-// static void
-// _binder_inner_proc_lock(struct binder_proc *proc, int line)
-// 	__acquires(&proc->inner_lock)
-// {
-// 	spin_lock(&proc->inner_lock);
-// }
+#define binder_inner_proc_lock(proc) _binder_inner_proc_lock(proc, __LINE__)
+static void
+_binder_inner_proc_lock(struct binder_proc *proc, int line)
+	__acquires(&proc->inner_lock)
+{
+	spin_lock(&proc->inner_lock);
+}
 
 /**
  * binder_inner_proc_unlock() - Release inner lock for given binder_proc
@@ -45,13 +49,13 @@
  *
  * Release lock acquired via binder_inner_proc_lock()
  */
-// #define binder_inner_proc_unlock(proc) _binder_inner_proc_unlock(proc, __LINE__)
-// static void
-// _binder_inner_proc_unlock(struct binder_proc *proc, int line)
-// 	__releases(&proc->inner_lock)
-// {
-// 	spin_unlock(&proc->inner_lock);
-// }
+#define binder_inner_proc_unlock(proc) _binder_inner_proc_unlock(proc, __LINE__)
+static void
+_binder_inner_proc_unlock(struct binder_proc *proc, int line)
+	__releases(&proc->inner_lock)
+{
+	spin_unlock(&proc->inner_lock);
+}
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0))
 static long __copy_from_user_nofault(void *dst, const void __user *src, size_t size)
@@ -188,6 +192,15 @@ void binder_alloc_handler(void *data, size_t size, struct binder_alloc *alloc, i
 }
 #endif
 
+void binder_preset_handler(void *data, struct hlist_head *hhead, struct mutex *lock)
+{
+	if (binder_procs == NULL)
+		binder_procs = hhead;
+
+	if (binder_procs_lock == NULL)
+		binder_procs_lock = lock;
+}
+
 struct moto_binder_transaction_log_entry {
 	int seq_id;
 	int call_type;
@@ -244,6 +257,27 @@ void moto_binder_write_status(int call_type, int caller_uid, int caller_pid, int
 	moto_binder_send_uevent(call_type, caller_uid, caller_pid, target_uid, target_pid, arg1, arg2);
 }
 
+static char* binder_type_to_string(int type)
+{
+	switch (type)
+	{
+	case CALL_BINDER:
+		return "call";
+
+	case ASYNC_BINDER:
+		return "async";
+
+	case REPLY_BINDER:
+		return "reply";
+
+	case ASYNC_ALLOC_FULL:
+		return "async_buf_full";
+
+	default:
+		return "unknown";
+	}
+}
+
 int proc_binder_status_show(struct seq_file *m, void *unused)
 {
 	struct moto_binder_transaction_log *log = &moto_binder_transaction_log;
@@ -263,8 +297,7 @@ int proc_binder_status_show(struct seq_file *m, void *unused)
 
 		seq_printf(m,
 		   "%d: %s from %d:%d to %d:%d %d %d\n",
-		   e->seq_id, (e->call_type == REPLY_BINDER) ? "reply" :
-		   ((e->call_type == ASYNC_BINDER) ? "async" : "call "),
+		   e->seq_id, binder_type_to_string(e->call_type),
 		   e->caller_uid, e->caller_pid, e->target_uid, e->target_pid,
 		   e->arg1, e->arg2);
 
@@ -272,9 +305,174 @@ int proc_binder_status_show(struct seq_file *m, void *unused)
 	return 0;
 }
 
+static void get_uid_pid(int *from_pid, int *from_uid, int *to_pid, int *to_uid, struct binder_transaction *tr)
+{
+	/* from pid/uid */
+	*from_pid = -1;
+	*from_uid = tr->sender_euid.val;
+	if (tr->from != NULL && tr->from->task != NULL) {
+		*from_pid = task_tgid_nr(tr->from->task);
+	} else if (tr->from != NULL &&
+		   tr->from->proc != NULL &&
+		   tr->from->proc->tsk != NULL) {
+		*from_pid = task_tgid_nr(tr->from->proc->tsk);
+	} else if (tr->from != NULL && tr->from->proc != NULL) {
+		*from_pid = tr->from->proc->pid;
+	} else if (tr->from != NULL) {
+		*from_pid = tr->from->pid;
+	}
+
+	/* to thread pid/uid */
+	*to_pid = -1;
+	if (tr->to_thread != NULL && tr->to_thread->task != NULL) {
+		*to_pid = task_tgid_nr(tr->to_thread->task);
+	} else if (tr->to_thread != NULL &&
+		   tr->to_thread->proc != NULL &&
+		   tr->to_thread->proc->tsk != NULL) {
+		*to_pid = task_tgid_nr(tr->to_thread->proc->tsk);
+	} else if (tr->to_proc != NULL && tr->to_proc->tsk != NULL) {
+		*to_pid = task_tgid_nr(tr->to_proc->tsk);
+	}
+}
+
+static bool binder_worklist_empty_ilocked(struct list_head *list)
+{
+	return list_empty(list);
+}
+
+void proc_binder_check_status_print(struct seq_file *m, int caller_uid, int caller_pid, int target_uid, int target_pid, int arg1, int arg2)
+{
+	seq_printf(m,
+		"from %d:%d to %d:%d %d %d\n",
+		caller_uid, caller_pid, target_uid, target_pid,
+		arg1, arg2);
+}
+
+void proc_binder_check_status_show_detail(struct seq_file *m, struct binder_proc *proc, int to_uid)
+{
+	struct rb_node *n = NULL;
+	struct binder_thread *thread = NULL;
+	int from_uid = -1;
+	int from_pid = -1;
+	int to_pid = -1;
+	struct binder_transaction *btrans = NULL;
+	bool empty = true;
+	int need_reply = -1;
+	struct binder_work *w = NULL;
+
+	/*check binder_thread/transaction_stack/binder_proc ongoing transaction*/
+	binder_inner_proc_lock(proc);
+	for (n = rb_first(&proc->threads); n != NULL; n = rb_next(n)) {
+		thread = rb_entry(n, struct binder_thread, rb_node);
+		empty = binder_worklist_empty_ilocked(&thread->todo);
+
+		if (thread->task != NULL) {
+			/*has "todo" binder thread in worklist?*/
+			if (!empty) {
+				/*scan thread->todo list*/
+				list_for_each_entry(w, &thread->todo, entry) {
+					btrans = container_of(w, struct binder_transaction, work);
+					if (w != NULL && w->type == BINDER_WORK_TRANSACTION && btrans != NULL) {
+						spin_lock(&btrans->lock);
+						get_uid_pid(&from_pid, &from_uid, &to_pid, &to_uid, btrans);
+						need_reply = (int)(!(btrans->flags & TF_ONE_WAY));
+						if (btrans->to_thread != NULL && btrans->to_thread == thread && (!(btrans->flags & TF_ONE_WAY))) {
+							spin_unlock(&btrans->lock);
+							if (from_uid != to_uid) {
+								binder_inner_proc_unlock(proc);
+								proc_binder_check_status_print(m, from_uid, from_pid, to_uid, to_pid, 0, need_reply);
+								return;
+							}
+						} else {
+							spin_unlock(&btrans->lock);
+						}
+					} else if (w != NULL && w->type != BINDER_WORK_TRANSACTION_COMPLETE && w->type != BINDER_WORK_NODE) {
+						/* under binder error/dead status, to_proc could be null, user "-1" instead uid/pid */
+						binder_inner_proc_unlock(proc);
+						proc_binder_check_status_print(m, -1, -1, to_uid, -1, 1, 1);
+						return;
+					}
+				}
+			}
+
+			/*has transcation in transaction_stack?*/
+			btrans = thread->transaction_stack;
+			if (btrans) {
+				spin_lock(&btrans->lock);
+				if (btrans->to_thread != NULL && btrans->to_thread == thread) {
+					/*only report incoming binder call*/
+					need_reply = (int)(!(btrans->flags & TF_ONE_WAY));
+					get_uid_pid(&from_pid, &from_uid, &to_pid, &to_uid, btrans);
+					spin_unlock(&btrans->lock);
+					if (from_uid != to_uid) {
+						binder_inner_proc_unlock(proc);
+						proc_binder_check_status_print(m, from_uid, from_pid, to_uid, to_pid, 2, need_reply);
+						return;
+					}
+				} else {
+					spin_unlock(&btrans->lock);
+				}
+			}
+		}
+	}
+
+	/*has "todo" binder proc in worklist*/
+	empty = binder_worklist_empty_ilocked(&proc->todo);
+	if (proc->tsk != NULL && !empty) {
+		list_for_each_entry(w, &proc->todo, entry) {
+			btrans = container_of(w, struct binder_transaction, work);
+			if (w != NULL && w->type == BINDER_WORK_TRANSACTION && btrans != NULL) {
+				spin_lock(&btrans->lock);
+				get_uid_pid(&from_pid, &from_uid, &to_pid, &to_uid, btrans);
+				need_reply = (int)(!(btrans->flags & TF_ONE_WAY));
+				if (btrans->to_thread != NULL && btrans->to_thread == thread && (!(btrans->flags & TF_ONE_WAY))) {
+					spin_unlock(&btrans->lock);
+					if (from_uid != to_uid) {
+						binder_inner_proc_unlock(proc);
+						proc_binder_check_status_print(m, from_uid, from_pid, to_uid, to_pid, 3, need_reply);
+						return;
+					}
+				} else {
+					spin_unlock(&btrans->lock);
+				}
+			} else if (w != NULL && w->type != BINDER_WORK_TRANSACTION_COMPLETE && w->type != BINDER_WORK_NODE) {
+				/* under binder error/dead status, to_proc could be null, user "-1" instead uid/pid */
+				binder_inner_proc_unlock(proc);
+				proc_binder_check_status_print(m, -1, -1, to_uid, -1, 4, 1);
+				return;
+			}
+		}
+	}
+	binder_inner_proc_unlock(proc);
+}
+
+int proc_binder_check_status_show(struct seq_file *m, void *unused)
+{
+	struct binder_proc *proc;
+
+	if (moto_binder_check_uid <= MIN_USERAPP_UID)
+		return 0;
+
+	mutex_lock(binder_procs_lock);
+	hlist_for_each_entry(proc, binder_procs, proc_node) {
+		if (proc != NULL && (proc->tsk != NULL) && (task_uid(proc->tsk).val == moto_binder_check_uid)) {
+			proc_binder_check_status_show_detail(m, proc, moto_binder_check_uid);
+		}
+	}
+	mutex_unlock(binder_procs_lock);
+
+	return 0;
+}
+
 int register_moto_binder_hooks(void)
 {
 	int rc = 0;
+
+	rc = register_trace_android_vh_binder_preset(binder_preset_handler, NULL);
+	if (rc != 0) {
+		pr_err("register_trace_android_vh_binder_preset failed, rc=%d\n", rc);
+		return rc;
+	}
 
 	rc = register_trace_android_vh_binder_trans(binder_trans_handler, NULL);
 	if (rc != 0) {
@@ -298,6 +496,7 @@ int register_moto_binder_hooks(void)
 
 void unregister_moto_binder_hooks(void)
 {
+	unregister_trace_android_vh_binder_preset(binder_preset_handler, NULL);
 	unregister_trace_android_vh_binder_trans(binder_trans_handler, NULL);
 	unregister_trace_android_vh_binder_reply(binder_reply_handler, NULL);
 	unregister_trace_android_vh_binder_alloc_new_buf_locked(binder_alloc_handler, NULL);
--- drivers/moto_binder/moto_binder_sysfs.c
+++ drivers/moto_binder/moto_binder_sysfs.c
@@ -26,6 +26,8 @@
 static int moto_binder_status_enabled = 0;
 static struct proc_dir_entry *d_moto_binder;
 
+int moto_binder_check_uid = 0;
+
 static ssize_t proc_binder_status_enabled_write(struct file *file, const char __user *buf,
 		size_t count, loff_t *ppos)
 {
@@ -105,6 +107,80 @@ static const struct proc_ops proc_binder_status_fops = {
 	.proc_release	= seq_release,
 };
 
+static ssize_t proc_binder_check_uid_write(struct file *file, const char __user *buf,
+		size_t count, loff_t *ppos)
+{
+	char buffer[13];
+	int err, val;
+
+	memset(buffer, 0, sizeof(buffer));
+
+	if (count > sizeof(buffer) - 1)
+		count = sizeof(buffer) - 1;
+
+	if (copy_from_user(buffer, buf, count))
+		return -EFAULT;
+
+	buffer[count] = '\0';
+	err = kstrtoint(strstrip(buffer), 10, &val);
+	if (err)
+		return err;
+
+	moto_binder_check_uid = val;
+
+	return count;
+}
+
+static ssize_t proc_binder_check_uid_read(struct file *file, char __user *buf,
+		size_t count, loff_t *ppos)
+{
+	char buffer[13];
+	size_t len = 0;
+
+	len = snprintf(buffer, sizeof(buffer), "%d\n", moto_binder_check_uid);
+
+	return simple_read_from_buffer(buf, count, ppos, buffer, len);
+}
+
+static const struct proc_ops proc_binder_check_uid_fops = {
+	.proc_write		= proc_binder_check_uid_write,
+	.proc_read		= proc_binder_check_uid_read,
+};
+
+static void *proc_binder_check_status_start(struct seq_file *m, loff_t *pos)
+{
+	return *pos < 1 ? (void *)1 : NULL;
+}
+
+static void *proc_binder_check_status_next(struct seq_file *m, void *v, loff_t *pos)
+{
+	++*pos;
+	return NULL;
+}
+
+static void proc_binder_check_status_stop(struct seq_file *m, void *v)
+{
+}
+
+const struct seq_operations proc_binder_check_status_op = {
+	.start	= proc_binder_check_status_start,
+	.next	= proc_binder_check_status_next,
+	.stop	= proc_binder_check_status_stop,
+	.show	= proc_binder_check_status_show
+};
+
+static int proc_binder_check_status_open(struct inode *inode, struct file *file)
+{
+	return seq_open(file, &proc_binder_check_status_op);
+}
+
+static const struct proc_ops proc_binder_check_status_fops = {
+	.proc_open	= proc_binder_check_status_open,
+	.proc_read	= seq_read,
+	.proc_lseek	= seq_lseek,
+	.proc_release	= seq_release,
+};
+
 static int __init moto_binder_proc_init(void)
 {
 	struct proc_dir_entry *proc_node;
@@ -127,8 +203,27 @@ static int __init moto_binder_proc_init(void)
 		goto err_create_binder_status;
 	}
 
+	proc_node = proc_create("binder_check_uid", 0666, d_moto_binder, &proc_binder_check_uid_fops);
+	if (!proc_node) {
+		pr_err("failed to create proc node binder_check_uid\n");
+		goto err_creat_binder_check_uid;
+	}
+
+	proc_node = proc_create("binder_check_status", 0444, d_moto_binder, &proc_binder_check_status_fops);
+	if (!proc_node) {
+		pr_err("failed to create proc node binder_check_status\n");
+		goto err_create_binder_check_status;
+	}
+
 	return 0;
 
+
+err_create_binder_check_status:
+
+	remove_proc_entry("binder_check_uid", NULL);
+err_creat_binder_check_uid:
+
+	remove_proc_entry("binder_status", NULL);
 err_create_binder_status:
 
 	remove_proc_entry("binder_status_enabled", NULL);
