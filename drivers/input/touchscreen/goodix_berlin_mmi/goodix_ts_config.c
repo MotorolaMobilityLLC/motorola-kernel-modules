43c1ee4a3b61767447ed10886a14e3aa8f358e97
Xiaojun Lv <lvxj4@motorola.com>
2024-10-08 11:52:16 +0800

    mona: support touch sensitivity in game mode
    Support touch sensitivity in game mode for mona/monai,
    default touch report rate is 240Hz and high report rate is 360Hz.
    
    Change-Id: I1659e5e65de32d244569497e7f5ff39443035027
    Reviewed-on: https://gerrit.mot.com/3105994
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/goodix_berlin_mmi/goodix_ts_config.c
+++ drivers/input/touchscreen/goodix_berlin_mmi/goodix_ts_config.c
@@ -17,7 +17,7 @@ struct goodix_ic_report_rate_config report_rate_config_info = {
 	||defined(PRODUCT_EQS) || defined(PRODUCT_RTWO) || defined(PRODUCT_BRONCO) \
 	|| defined(PRODUCT_ZEEKR) || defined(PRODUCT_LYNKCO) || defined(PRODUCT_BANGKK) \
 	|| defined(PRODUCT_EQE) || defined(PRODUCT_CTWO) || defined(PRODUCT_BOSTON) \
-	|| defined(PRODUCT_AVATRN) || defined(PRODUCT_CUSCO) \
+	|| defined(PRODUCT_AVATRN) || defined(PRODUCT_CUSCO) || defined(PRODUCT_MONA) \
 	|| defined(PRODUCT_MALMO) || defined(PRODUCT_TANK) || defined(PRODUCT_LEAP)
 	.rate_config_count = 3,
 	.refresh_rate_ctrl = 0,
