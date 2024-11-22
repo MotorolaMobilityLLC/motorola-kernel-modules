93ae45a54aa0c747264932036bb8cf94265bacbd
huangzq2 <huangzq2@motorola.com>
2024-11-22 17:11:03 +0800

    clean up the moto_mm
    remove lru shrinker as it was not used
    
    Change-Id: Ifb413660c88086a80bf00ca786b921618fdf4ecc
    Signed-off-by: huangzq2 <huangzq2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3151766
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>

--- drivers/moto_mm/mm_main.c
+++ drivers/moto_mm/mm_main.c
@@ -43,18 +43,9 @@ static void drain_all_pages_bypass_hook(void *data, gfp_t gfp_mask, unsigned int
 }
 #endif
 
-// 5.10 - 5.15 skip pages mapcount >= 20
-#if defined(MAPPED_PROTECTOR_SUPPORTED)
-static void page_referenced_check_bypass_hook(void *data, struct page *page, unsigned long nr_to_scan, int lru, bool *bypass)
-{
-	if ((atomic_read(&page->_mapcount) + 1) >= 20)
-		*bypass = true;
-}
-#endif
-
 static int register_all_hooks(void)
 {
-#if defined(TUNE_INACTIVE_SUPPORTED) || defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED) || defined(MAPPED_PROTECTOR_SUPPORTED)
+#if defined(TUNE_INACTIVE_SUPPORTED) || defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED)
 	int rc;
 
 #if defined(TUNE_INACTIVE_SUPPORTED)
@@ -63,15 +54,8 @@ static int register_all_hooks(void)
 #if defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED)
 	REGISTER_HOOK(drain_all_pages_bypass);
 #endif // defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED)
-#if defined(MAPPED_PROTECTOR_SUPPORTED)
-	REGISTER_HOOK(page_referenced_check_bypass);
-#endif // defined(MAPPED_PROTECTOR_SUPPORTED)
 	return 0;
 
-#if defined(MAPPED_PROTECTOR_SUPPORTED)
-	UNREGISTER_HOOK(page_referenced_check_bypass);
-ERROR_OUT(page_referenced_check_bypass):
-#endif // defined(MAPPED_PROTECTOR_SUPPORTED)
 #if defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED)
 	UNREGISTER_HOOK(drain_all_pages_bypass);
 ERROR_OUT(drain_all_pages_bypass):
@@ -84,14 +68,11 @@ ERROR_OUT(tune_inactive_ratio):
 
 #else
 	return 0;
-#endif // defined(TUNE_INACTIVE_SUPPORTED) || defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED) || defined(MAPPED_PROTECTOR_SUPPORTED)
+#endif // defined(TUNE_INACTIVE_SUPPORTED) || defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED)
 }
 
 static void unregister_all_hooks(void)
 {
-#if defined(MAPPED_PROTECTOR_SUPPORTED)
-	UNREGISTER_HOOK(page_referenced_check_bypass);
-#endif // defined(MAPPED_PROTECTOR_SUPPORTED)
 #if defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED)
 	UNREGISTER_HOOK(drain_all_pages_bypass);
 #endif // defined(DRAIN_ALL_PAGES_BYPASS_SUPPORTED)
@@ -117,21 +98,12 @@ static int __init moto_mm_init(void)
 		mm_info_init();
 #endif // defined(MM_INFO_SUPPORTED)
 
-#if defined(LRU_SHRINKER_SUPPORTED)
-	if (moto_lru_shrinker_enabled > 0)
-		mm_lru_shrinker_init();
-#endif // defined(LRU_SHRINKER_SUPPORTED)
-
 	pr_info("moto_mm_init succeed!\n");
 	return 0;
 }
 
 static void __exit moto_mm_exit(void)
 {
-#if defined(LRU_SHRINKER_SUPPORTED)
-	mm_lru_shrinker_exit();
-#endif // defined(LRU_SHRINKER_SUPPORTED)
-
 #if defined(MM_INFO_SUPPORTED)
 	mm_info_exit();
 #endif // defined(MM_INFO_SUPPORTED)
