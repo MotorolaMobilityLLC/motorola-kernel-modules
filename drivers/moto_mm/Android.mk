0c8ba0040118c5ba4abdcf7e073d7a1161a3da9f
lixh8 <lixh8@lenovo.com>
2024-12-18 09:06:52 +0800

    feature: motostats driver for kernel 6.1
    feature:
    motostats driver for kernel 6.1
    
    Change-Id: I78fb718cac438d26a524d9c4d06ba4c2be14f7e8
    Signed-off-by: lixh8 <lixh8@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3175471
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wang Wang <wangwang@motorola.com>
    Submit-Approved: Jira Key

--- drivers/moto_mm/Android.mk
+++ drivers/moto_mm/Android.mk
@@ -1,6 +1,6 @@
 DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
-
+SRC_PATH := $(LOCAL_PATH)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := moto_mm.ko
@@ -9,4 +9,6 @@ LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
 
 include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(SRC_PATH)/stat/mm/Android.mk
 
