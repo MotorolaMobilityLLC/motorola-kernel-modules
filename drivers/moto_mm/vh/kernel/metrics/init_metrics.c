6cdcece9756f4ba6b2e5c72644e405faddec1128
lixh8 <lixh8@lenovo.com>
2025-01-09 13:00:56 +0800

    feature: moto metrics driver for kernel 6.1
    feature:
    moto metrics driver for for kernel 6.1
    
    Change-Id: Ib713230a866afae93e65b289524b62e08f5b42d0
    Signed-off-by: lixh8 <lixh8@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3197217
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wang Wang <wangwang@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/moto_mm/vh/kernel/metrics/init_metrics.c
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/* init_metrics.c
+ *
+ * Support for init metrics
+ * Copyright 2022 Moto LLC
+ */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kobject.h>
+
+extern int thermal_metrics_init(struct kobject *metrics_kobj);
+extern int perf_metrics_init(struct kobject *metrics_kobj);
+
+struct kobject *metrics_kobj;
+
+static int __init metrics_init(void)
+{
+	metrics_kobj = kobject_create_and_add("metrics", kernel_kobj);
+	thermal_metrics_init(metrics_kobj);
+	perf_metrics_init(metrics_kobj);
+	return 0;
+}
+EXPORT_SYMBOL_GPL(metrics_kobj);
+module_init(metrics_init);
+MODULE_LICENSE("GPL v2");
