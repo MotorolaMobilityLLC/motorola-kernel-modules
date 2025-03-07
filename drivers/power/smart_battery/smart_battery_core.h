d75206e99105d7e6db5236f7bb67c457c4763b44
liuyc20 <liuyc20@lenovo.com>
2025-03-07 14:47:41 +0800

    smart_battery: uiSOH = batt_soh + soh_gap
    - From the analysis of the collected user data, the battery soh reported
    by fuel gauge is about 4% lower than the true SOH.
    - the Manufacturers of cw2217 gauge also confirm that there is a 4% gap in gauge output soh
      other gauge soh gap about 3%-5%
    -In order for the user to see a more realistic SOH,
    uisoh needs to add this soh_gap.
    
    Change-Id: I131a3cff804d84be11f686fc03d7df5c15cafe48
    Signed-off-by: liuyc20 <liuyc20@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3295062
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/smart_battery/smart_battery_core.h
+++ drivers/power/smart_battery/smart_battery_core.h
@@ -94,6 +94,7 @@ struct mmi_smart_battery {
 	int				combo_charge_counter;
 	int				combo_soh;
 	int				combo_cycle_count;
+	int				soh_gap;
 	int				shutdown_threshold;
 	int				soc100_curr_threshod;
 	int				taper_count;
@@ -131,6 +132,7 @@ struct mmi_smart_battery {
 #define DEFAULT_VBATT_LOW_MV		3000
 #define DEFAULT_VBATT_LOW_COLD_MV	3000
 #define DEFAULT_BATT_COLD_THRESHOLD	0
+#define DEFAULT_SOH_GAP			4
 
 extern int mmi_batt_health_check(void);
 
