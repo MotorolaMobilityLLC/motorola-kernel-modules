1b83fbe8dba506234db1af2253cfbfbc245559ba
149 <longc149@motorola.com>
2025-03-10 20:10:52 +0800

    Noida sensor parm update 3
    PROPAGATED_from (CR)
    
    modify capsensor  parameter
    modify awinic parm
    
    Change-Id: I735b873fd7131d4123b71a4fe68e553fc55a5311
    Signed-off-by: 149 <longc149@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3298227
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yuchang Guo <guoyc1@lenovo.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Reviewed-by: Sen Ling <lingsen2@motorola.com>
    Submit-Approved: Sen Ling <lingsen2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3684640
    Reviewed-by: Andrea dos Santos <andreads@motorola.com>
    Submit-Approved: Andrea dos Santos <andreads@motorola.com>

--- include/linux/input/awinic/aw_sar_type.h
+++ include/linux/input/awinic/aw_sar_type.h
@@ -121,6 +121,8 @@ struct aw_sar_dts_info {
 	bool use_pm;
 	bool update_fw_flag;
 	bool use_plug_cail_flag;
+	bool new_fw_format_flag;
+	const char *plat_state[1];
 	/*wxm modify start by 2023/12/5*/
 	uint32_t monitor_esd_flag;
 	/*wxm modify end by 2023/12/5*/
