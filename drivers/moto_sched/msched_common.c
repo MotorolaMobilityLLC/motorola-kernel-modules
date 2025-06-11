ab782a8595c800edb817cb45fbc363137c1a1524
lihaizhou <lihz21@motorola.com>
2025-06-11 18:07:35 +0800

    Add Ftrace support for moto_sched
    Mot-CR-fixed:(CR)
    
    Change-Id: I24688ab1a3bcc40b70728031e12f8fdf7c2fcc23
    Signed-off-by: lihaizhou <lihz21@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3427051
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 45c4af326051c882bfd0687d05fbdf16d43f64a8)
    Reviewed-on: https://gerrit.mot.com/3570875
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Bingqian Yang <yangbq1@lenovo.com>
    Reviewed-by: Jichao Zou <zoujc@motorola.com>

--- drivers/moto_sched/msched_common.c
+++ drivers/moto_sched/msched_common.c
@@ -34,6 +34,7 @@
 #if IS_ENABLED(CONFIG_MOTO_ENABLE_MDPF)
 #include "msched_uclamp.h"
 #endif
+#include "msched_trace.h"
 
 #define MS_TO_NS (1000000)
 #define MAX_INHERIT_GRAN ((u64)(64 * MS_TO_NS))
@@ -207,6 +208,9 @@ int task_get_mvp_prio(struct task_struct *p, bool with_inherit)
 		"pid=%d tgid=%d prio=%d scene=%d ux_type=%d task_util=%lu mvp_prio=%d\n",
 		p->pid, p->tgid, p->prio, moto_sched_scene, ux_type, moto_task_util(p), prio);
 
+	if (trace_msched_task_get_mvp_prio_enabled()) {
+		trace_msched_task_get_mvp_prio(p, ux_type, prio, moto_task_util(p), moto_sched_scene);
+	}
 	return prio;
 }
 EXPORT_SYMBOL(task_get_mvp_prio);
