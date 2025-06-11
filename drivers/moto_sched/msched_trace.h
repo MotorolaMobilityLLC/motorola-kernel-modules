64f8f21e61813b0de98ed6fa5381d5b06b429029
lihaizhou <lihz21@motorola.com>
2025-06-11 18:07:35 +0800

    Add Ftrace support for moto_sched
    Mot-CR-fixed:(CR)
    
    Change-Id: I24688ab1a3bcc40b70728031e12f8fdf7c2fcc23
    Signed-off-by: lihaizhou <lihz21@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3427051
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 45c4af326051c882bfd0687d05fbdf16d43f64a8)
    Reviewed-on: https://gerrit.mot.com/3570875
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Bingqian Yang <yangbq1@lenovo.com>
    Reviewed-by: Jichao Zou <zoujc@motorola.com>

--- drivers/moto_sched/msched_trace.h
+++ drivers/moto_sched/msched_trace.h
@@ -90,6 +90,36 @@ TRACE_EVENT(msched_uclamp_inheritance_result,
 		__entry->inherited_min, __entry->inherited_max, __entry->type)
 );
 
+TRACE_EVENT(msched_task_get_mvp_prio,
+        TP_PROTO(struct task_struct *p, int ux_type, int prio_val, unsigned long util, int scene),
+
+        TP_ARGS(p, ux_type, prio_val, util, scene),
+
+        TP_STRUCT__entry(
+                __field(pid_t, pid)
+                __field(pid_t, tgid)
+                __field(int, prio)
+                __field(int, ux_type)
+                __field(unsigned long, util)
+                __field(int, mvp_prio)
+                __field(int, scene)
+        ),
+
+        TP_fast_assign(
+                __entry->pid = p->pid;
+                __entry->tgid = p->tgid;
+                __entry->prio = p->prio;
+                __entry->ux_type = ux_type;
+                __entry->util = util;
+                __entry->mvp_prio = prio_val;
+                __entry->scene = scene;
+        ),
+
+        TP_printk("pid=%d tgid=%d prio=%d scene=%d ux_type=%d task_util=%lu mvp_prio=%d",
+                __entry->pid, __entry->tgid, __entry->prio, __entry->scene,
+                __entry->ux_type, __entry->util, __entry->mvp_prio)
+);
+
 #endif /* _TRACE_MSCHED_H */
 
 #undef TRACE_INCLUDE_PATH
