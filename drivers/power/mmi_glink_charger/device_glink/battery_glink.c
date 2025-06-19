1737844b90454b58a66985994233f59fe7cebf31
xuwt2 <xuwt2@lenovo.com>
2025-06-19 18:31:11 +0800

    mmi_glink_charger: Disable charge when battery is not present
    In order to ensure the safety of the battery
    Disable battery charge when battery is not present.
    
    Change-Id: I38b0a0300b2aeb0476f8cb24c10d8713bf9b37c9
    Signed-off-by: xuwt2 <xuwt2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3437635
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wentao Xu <xuwt2@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Reviewed-by: Lei Chen <chenl18@motorola.com>
    Reviewed-by: Wei Xu <xuwei9@lenovo.com>
    Submit-Approved: Jira Key

--- drivers/power/mmi_glink_charger/device_glink/battery_glink.c
+++ drivers/power/mmi_glink_charger/device_glink/battery_glink.c
@@ -467,9 +467,9 @@ static int battery_notify_handler(struct notifier_block *nb, unsigned long event
 		}
 
 		mmi_info(this_root_chip, "Batt_dev[%d]: batt_uv %d, batt_ua %d, "
-							"batt_soc %d, batt_temp %d, batt_status %d, batt_soh %d",
+							"batt_soc %d, batt_temp %d, batt_status %d, batt_soh %d, batt_present %d",
 							batt_chip->batt_role, batt_info.batt_uv, batt_info.batt_ua, batt_info.batt_soc,
-							batt_info.batt_temp, batt_info.batt_status, batt_info.batt_soh);
+							batt_info.batt_temp, batt_info.batt_status, batt_info.batt_soh, batt_info.present);
 
 		mmi_info(this_root_chip, "Batt_dev[%d]: batt_cycle %d, batt_full_uah %d, "
 							"batt_design_uah %d, batt_chg_counter %d, batt_fv_uv %d, batt_fcc_ua %d",
