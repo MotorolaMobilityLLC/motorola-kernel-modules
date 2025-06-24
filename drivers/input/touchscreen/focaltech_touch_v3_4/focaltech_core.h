9e5ca6238aa74969c5fadb37f6f402879796a7c7
dengwei <dengwei1@motorola.com>
2025-06-24 14:08:07 +0800

    roadstr: input touch add 2nd focal support
    input touch
    add 2nd focal support
    
    Change-Id: Ib19b845f40a04efb31fc12b505c8f6efd6f2ca11
    Signed-off-by: dengwei <dengwei1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3444466
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit d02f0c4b9fb64b8b234850f86763e8205b64d2fa)
    Reviewed-on: https://gerrit.mot.com/3465529
    Reviewed-by: Gilberto Gambugge Neto <gambugge@motorola.com>

--- drivers/input/touchscreen/focaltech_touch_v3_4/focaltech_core.h
+++ drivers/input/touchscreen/focaltech_touch_v3_4/focaltech_core.h
@@ -72,6 +72,9 @@
 #ifdef CONFIG_FTS_LAST_TIME
 #include <linux/ktime.h>
 #endif
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 6, 30)
+#include <linux/pinctrl/consumer.h>
+#endif
 
 /*****************************************************************************
 * Private constant and macro definitions using #define
