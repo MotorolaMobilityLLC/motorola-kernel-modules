5242175dadaa13b8c188ab513f475ae0a1b4252e
lixh8 <lixh8@lenovo.com>
2025-07-28 22:29:28 +0800

    kernel 5.15: Add moto_stat.ko driver for memory reclaim monitoring
    This change introduces a new kernel module `moto_stat.ko` that monitors 
    memory reclaim operations by collecting:
    
    **Key metrics collected**:
       - Number of memory reclaim cycles 
       - Time spent in reclaim operations 
    
    **Monitoring benefits**:
       - Enables real-time tracking of memory pressure and system responsiveness
       - Helps identify memory leaks and inefficient allocation patterns
       - Provides data for performance optimization and thermal management
    
    This driver supports proactive system health monitoring and performance 
    tuning by providing visibility into memory management efficiency.
    
    Change-Id: Id509cdb9a5d70671a00f53f7b1a10035afdf7446
    Signed-off-by: lixh8 <lixh8@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3482633
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Wang Wang <wangwang@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-by: Obi-Wan Kenobi

--- drivers/moto_mm/Android.mk
+++ drivers/moto_mm/Android.mk
@@ -11,5 +11,6 @@ KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(SRC_PATH)/stat/mm/Android.mk
+include $(SRC_PATH)/stat/mm515/Android.mk
 include $(SRC_PATH)/vh/kernel/metrics/Android.mk
 
