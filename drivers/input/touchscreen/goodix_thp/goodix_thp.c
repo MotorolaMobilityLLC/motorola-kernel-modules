87cefdf2e1350a18384d9a95c91cd71a60af250c
Wenge Zhu <zhuwg1@motorola.com>
2025-07-29 10:57:43 +0800

    Fix thp driver stability issue
    1. When device boots up, kernel panic generated.
    
    Reason:
    The problem lies in that 'core_data->cpu_to_index_map' is not
    initialized correctly and its value is NULL.
    
    Solution:
    We should ensure that the interrupt is not triggered until the driver
    initialization is completed. So, it is best to register for an interrupt
    after the initialization is completed.
    
    kernel panic log:
    <6>[   24.271884][   T14] goodix_thp_drvier spi19.0: [THP-INF] [goodix_thp_irq_setup:1303] IRQ:439,flags:8194
    <6>[   24.279216][ T1313] goodix_thp_drvier spi19.0: [THP-INF] [goodix_thp_threadirq_func:1163] CPU Athene set to mask 0xf0
    <1>[   24.279224][ T1313] Unable to handle kernel NULL pointer dereference at virtual address 0000000000000010
    ......
    <6>[   24.279857][ T1313] Call trace:
    <6>[   24.279858][ T1313]  goodix_thp_threadirq_func+0x5c/0x884 [goodix_brl_thp 55694f4052bff09f3455875fb23a3593ac7e3324]
    <6>[   24.279865][ T1313]  irq_thread_fn+0x48/0xa8
    <6>[   24.279868][ T1313]  irq_thread+0x130/0x1ec
    <6>[   24.279870][ T1313]  kthread+0x110/0x134
    <6>[   24.279873][ T1313]  ret_from_fork+0x10/0x20
    <0>[   24.279876][ T1313] Code: b9802916 71007edf 5400044c f9586a68 (b8767915)
    <4>[   24.279879][ T1313] ---[ end trace 0000000000000000 ]---
    
    2. Remove the CPU Athene that continuously assigns the kernel thread.
    
    Change-Id: Ib1b20b5a945905ef4f4bb081a2379386ddc05ba7
    Signed-off-by: Wenge Zhu <zhuwg1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3483446
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Jun Weng <wengjun1@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/goodix_thp/goodix_thp.c
+++ drivers/input/touchscreen/goodix_thp/goodix_thp.c
@@ -1156,7 +1156,6 @@ static irqreturn_t goodix_thp_threadirq_func(int irq, void *data)
         if (unlikely(!affinity_initialized)) {
             cpumask_clear(&cpumask);
             cpumask.bits[0] = core_data->ts_dev->board_data.cpu_mask;
-            set_cpus_allowed_ptr(current, &cpumask);
             if (set_cpus_allowed_ptr(current, &cpumask) != 0) {
                 ts_err(ts_dev->dev, "Failed to set CPU affinity");
             } else {
@@ -2771,6 +2770,21 @@ static int goodix_thp_probe(struct platform_device *pdev)
 
 #endif
 
+#ifdef CONFIG_ENABLE_TOUCH_CPU_BOOST
+        if (goodix_init_cpu_boost(pdev)) {
+                ts_err(tdev->dev, "CPU boost initialization failed");
+                core_data->qos_count = 0;
+        }
+
+        //init timer for clear boost_count
+        core_data->boost_count = 0;
+        timer_setup(&core_data->boost_timer, goodix_boost_timer_handler, 0);
+         ts_info(tdev->dev, "Touch boost config: affinity=0x%lx, boost-count=%d, timeout=%dms",
+                  tdev->board_data.cpu_mask,
+                  tdev->board_data.max_boost_count,
+                  tdev->board_data.boost_timeout);
+#endif
+
         /* request irq */
         r = goodix_thp_irq_setup(core_data);
         if (r) {
@@ -2795,21 +2809,6 @@ static int goodix_thp_probe(struct platform_device *pdev)
             ts_info(tdev->dev, "Failed register touchscreen mmi.");
             goto err_irq_setup;
         }
-#endif
-
-#ifdef CONFIG_ENABLE_TOUCH_CPU_BOOST
-        if (goodix_init_cpu_boost(pdev)) {
-                ts_err(tdev->dev, "CPU boost initialization failed");
-                core_data->qos_count = 0;
-        }
-
-        //init timer for clear boost_count
-        core_data->boost_count = 0;
-        timer_setup(&core_data->boost_timer, goodix_boost_timer_handler, 0);
-         ts_info(tdev->dev, "Touch boost config: affinity=0x%lx, boost-count=%d, timeout=%dms",
-                  tdev->board_data.cpu_mask,
-                  tdev->board_data.max_boost_count,
-                  tdev->board_data.boost_timeout);
 #endif
 
         return 0;
