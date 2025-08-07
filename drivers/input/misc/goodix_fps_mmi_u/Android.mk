2dd1d8af2d4a6e8767ba2e5b3078b1ce67022f87
ouyang feifan <ouyangff@motorola.com>
2025-08-07 16:39:50 +0800

    [Bogota W]2nd goodix bringup
    change:
    setup a new path for DDK change on K612
    
    Change-Id: I5d5fb8d97046ed5ad83d0b45ee103dd5c28584f1
    Signed-off-by: ouyang feifan <ouyangff@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3496793
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yan Zhang <zhangy26@motorola.com>
    Reviewed-by: Xinmin Fu <fuxm1@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/input/misc/goodix_fps_mmi_u/Android.mk
@@ -0,0 +1,19 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(MOTO_FPS_PRECISE_POWERON), true)
+    KERNEL_CFLAGS += CONFIG_MOTO_FPS_PRECISE_POWERON=y
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := goodix_fps_mmi_u.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+#ifneq ($(findstring mmi_relay.ko,$(BOARD_VENDOR_KERNEL_MODULES)),)
+#    KERNEL_CFLAGS += CONFIG_MMI_RELAY=y
+#    LOCAL_ADDITIONAL_DEPENDENCIES := $(KERNEL_MODULES_OUT)/mmi_relay.ko
+#    LOCAL_REQUIRED_MODULES := mmi_relay.ko
+#endif
+#KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
