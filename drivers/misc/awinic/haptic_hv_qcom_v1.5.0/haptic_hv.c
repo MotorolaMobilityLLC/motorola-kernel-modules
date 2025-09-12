43c8fcbde4287e267095672d107d95ed5e6326be
Lei Chen <chenlei18@lenovo.com>
2025-09-12 14:12:08 +0800

    haptic:add zhl call RTP vibration bin
    changes,
    add zhl call RTP vibration bin
    
    Change-Id: I5f6bf48ade0f074af32129c4ebce327430ecdd3f
    Signed-off-by: Lei Chen <chenlei18@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3551804
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wei Deng <dengwei1@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/awinic/haptic_hv_qcom_v1.5.0/haptic_hv.c
+++ drivers/misc/awinic/haptic_hv_qcom_v1.5.0/haptic_hv.c
@@ -74,7 +74,11 @@ char aw_rtp_name[][AW_RTP_NAME_MAX] = {
 	{"aw869xx_rtp_Moto_Retro.bin"},
 	{"aw869xx_rtp_Moto_Original.bin"},
 	{"aw869xx_rtp_Moto_Classic.bin"},
-	{"aw869xx_rtp_New_Hello_moto.bin"},
+	{"aw869xx_rtp_New_Hello_Moto.bin"},
+	{"aw8693x_rtp_CRICKET_RING.bin"},
+	{"aw8693x_rtp_Metro_Default.bin"},
+	{"aw8693x_rtp_Balinese_Camelan.bin"},
+	{"zlh_incomingcall_RTP.bin"},
 };
 
 #ifdef AW_TIKTAP
