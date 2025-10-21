0eaedc6a36ee1d2b1cc31543900ffe513c9864b1
Rafael Ortolan <rafones@motorola.com>
2025-10-21 18:29:24 +0000

    focaltech_0flash_v2_mmi: fix user build error at kernel 6.6
    API filp_open is not exported by the GKI at kernel 6.6, so this routine
    cannot be used by touchscreen drivers.
    
    PORTED FROM: (CR)
    
    Change-Id: I529fea6f73606f416ca5a1db52a4f5571993b197
    Reviewed-on: https://gerrit.mot.com/3600304
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Reviewed-by: Bruno Oliveira <brunosoe@motorola.com>
    Reviewed-by: Robert Cabral <robertc@motorola.com>
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Gilberto Gambugge Neto <gambugge@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/focaltech_0flash_v2_mmi/focaltech_flash.c
+++ drivers/input/touchscreen/focaltech_0flash_v2_mmi/focaltech_flash.c
@@ -733,7 +733,7 @@ err_fw_download:
     return ret;
 }
 
-
+#ifndef CONFIG_FTS_COMPATIBLE_WITH_GKI
 static int fts_read_file_default(char *file_name, u8 **file_buf)
 {
     int ret = 0;
@@ -786,6 +786,7 @@ static int fts_read_file_default(char *file_name, u8 **file_buf)
 
     return ret;
 }
+#endif
 
 static int fts_read_file_request_firmware(char *file_name, u8 **file_buf)
 {
@@ -829,7 +830,11 @@ static int fts_read_file(char *file_name, u8 **file_buf)
 
     ret = fts_read_file_request_firmware(file_name, file_buf);
     if (ret < 0) {
+#ifdef CONFIG_FTS_COMPATIBLE_WITH_GKI
+        ret = fts_read_file_request_firmware(file_name, file_buf);
+#else
         ret = fts_read_file_default(file_name, file_buf);
+#endif
         if (ret < 0) {
             FTS_ERROR("get fw file(default) fail");
             return ret;
