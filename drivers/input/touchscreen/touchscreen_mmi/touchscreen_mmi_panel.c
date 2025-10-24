e72300e77faeea6e9157da461ebe34a706f65b9c
litong <litong20@motorola.com>
2025-10-23 21:46:22 -0500

    Revert "(CR): touchscreen_mmi: Temporarily disable gestures mode in Vantage."
    This reverts commit 7909adb617c419db4438233663a5903e514a1878.
    CRs-fixed: (CR)
    
    Change-Id: Icaf5ffe0311879520c70b80be5d7615313f074f8
    Reviewed-on: https://gerrit.mot.com/3605784
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Reviewed-by: Obi-Wan Kenobi
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/touchscreen_mmi/touchscreen_mmi_panel.c
+++ drivers/input/touchscreen/touchscreen_mmi/touchscreen_mmi_panel.c
@@ -161,7 +161,7 @@ int ts_mmi_parse_dt(struct ts_mmi_dev *touch_cdev,
 		dev_info(DEV_TS, "%s: get fod_coords property x:%d y:%d\n", __func__,ppdata->fod_x,ppdata->fod_y);
 	}
 
-#if defined(CONFIG_BOARD_USES_DOUBLE_TAP_CTRL) && !defined(CONFIG_VANTAGE_DTB)
+#ifdef CONFIG_BOARD_USES_DOUBLE_TAP_CTRL
 	if (!of_property_read_u32(of_node, "mmi,supported_gesture_type", &ppdata->supported_gesture_type))
 		dev_info(DEV_TS, "%s: supported_gesture_type property %02x\n",
 				__func__, ppdata->supported_gesture_type);
