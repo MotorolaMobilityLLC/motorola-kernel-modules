2e32844d79616c637d15cc48cc6eaf2f36c02574
chenl3 <chenl3@motorola.com>
2025-10-29 10:48:11 +0800

    drivers:aw86927:Fix build without RICHTAP support
    Control by CONFIG-AAC-RICHTAP_SUPORT.Only projects that support richtap
    are allowed to use richtap_rtp_mode.Projects that do not support richtap
    will compile with errors.
    
    Change-Id: I18dab6b40775b72912c9751894908e599cac6094
    Signed-off-by: chenl3 <chenl3@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3613442
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Reviewed-by: Weilong Wu <wuwl10@motorola.com>
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Xinmin Fu <fuxm1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/awinic/aw86927/haptic_hv.c
+++ drivers/misc/awinic/aw86927/haptic_hv.c
@@ -2159,10 +2159,12 @@ static ssize_t activate_store(struct device *dev, struct device_attribute *attr,
 		aw_err("ram init failed, not allow to play!");
 		return count;
 	}
+#ifdef CONFIG_AAC_RICHTAP_SUPPORT
 	if (atomic_read(&aw_haptic->richtap_rtp_mode)) {
 		aw_info("In richtap rtp mode");
 		return count;
 	}
+#endif
 	mutex_lock(&aw_haptic->lock);
 	aw_haptic->state = val;
 	aw_haptic->activate_mode = aw_haptic->info.mode;
