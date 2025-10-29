3392858829700ffc9f0f0b674e41c03ba40d00d2
Yifan Xu <xuyf4@motorola.com>
2025-10-29 17:58:27 +0800

    add force enable node in sc8586
    Reason:
    for factory BT test
    Solution:
    add force cp enable node for factory test
    Test:
    node added
    
    Change-Id: I984e4c60b4db9c46193ea3f324a29885c26634c5
    Reviewed-on: https://gerrit.mot.com/3614250
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Ying Shen <shenying@motorola.com>
    Reviewed-by: Wang Wang <wangwang@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/sc8586_mmi/sc8586_charger.c
+++ drivers/power/sc8586_mmi/sc8586_charger.c
@@ -1207,10 +1207,59 @@ static ssize_t sc8586_test_store(struct device *dev,
 static DEVICE_ATTR(registers, 0660, sc8586_show_registers, sc8586_store_register);
 //static DEVICE_ATTR(sc8586_test, 0660, NULL, sc8586_test_store);
 
+static ssize_t show_force_chg_auto_enable(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	int ret;
+	int state = 0;
+	bool enable;
+	struct sc8586_chip *sc = dev_get_drvdata(dev);
+	if (!sc) {
+		pr_err("sc8586: chip not valid\n");
+		state = -ENODEV;
+		goto end;
+	}
+
+	ret = sc8586_check_charge_enabled(sc, &enable);
+	if (ret < 0) {
+		pr_err("sc8586: sc8586_is_chg_en not valid\n");
+		state = -ENODEV;
+		goto end;
+	}
+	state = enable;
+end:
+	return sprintf(buf, "%d\n", state);
+}
+
+static ssize_t store_force_chg_auto_enable(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+	int ret;
+	bool enable;
+	struct sc8586_chip *sc = dev_get_drvdata(dev);
+	if (!sc) {
+		pr_err("sc8586 chip not valid\n");
+		return -ENODEV;
+	}
+
+	enable = simple_strtoul(buf, NULL, 0);
+	ret = sc8586_enable_charge(sc, enable);
+	if (ret) {
+		pr_err("sc8586 Couldn't %s charging rc=%d\n",
+			   enable ? "enable" : "disable", (int)ret);
+		return ret;
+	}
+
+	pr_info("sc8586  %s charging \n",
+			   enable ? "enable" : "disable");
+
+	return count;
+}
+static DEVICE_ATTR(force_chg_auto_enable, 0664, show_force_chg_auto_enable, store_force_chg_auto_enable);
+
 static void sc8586_create_device_node(struct device *dev)
 {
     device_create_file(dev, &dev_attr_registers);
     //device_create_file(dev, &dev_attr_sc8586_test);
+    device_create_file(dev, &dev_attr_force_chg_auto_enable);
 }
 /********************creat devices note end*************************************************/
 
