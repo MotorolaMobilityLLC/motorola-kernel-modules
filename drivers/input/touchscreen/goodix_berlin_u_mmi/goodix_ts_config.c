e0322c402de685edf53cb7aa533203e8eef7b0c7
Jun Weng <wengjun1@motorola.com>
2025-11-12 14:58:57 +0800

    volcano: dlkm: Support report rate switching for goodix
    - Support switching between 130Hz and 360Hz touch
    report rates for goodix.
    
    Change-Id: I8caa02f053d39c0046b86ef7501271f135904996
    Signed-off-by: Jun Weng <wengjun1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3634274
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/goodix_berlin_u_mmi/goodix_ts_config.c
+++ drivers/input/touchscreen/goodix_berlin_u_mmi/goodix_ts_config.c
@@ -17,7 +17,8 @@ struct goodix_ic_report_rate_config report_rate_config_info = {
 	||defined(PRODUCT_EQS) || defined(PRODUCT_RTWO) || defined(PRODUCT_BRONCO) \
 	|| defined(PRODUCT_ZEEKR) || defined(PRODUCT_AION) || defined(PRODUCT_MANAUS) \
 	|| defined(PRODUCT_AITO) || defined(PRODUCT_SCOUT) || defined(PRODUCT_OULU) \
-	|| defined(PRODUCT_VIENNA) || defined(PRODUCT_CYBERT) || defined(PRODUCT_NAPLES)
+	|| defined(PRODUCT_VIENNA) || defined(PRODUCT_CYBERT) || defined(PRODUCT_NAPLES) \
+	|| defined(PRODUCT_VOLCANO)
 	.rate_config_count = 2,
 	.refresh_rate_ctrl = 0,
 	.interpolation_ctrl = 1,
