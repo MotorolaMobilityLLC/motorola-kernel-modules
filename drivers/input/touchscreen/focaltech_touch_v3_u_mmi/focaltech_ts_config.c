077af69a92ae594e19f61dd953407212022b4106
Jun Weng <wengjun1@motorola.com>
2025-12-03 16:26:30 +0800

    masera: dlkm: Support 130/300HZ report rate switching
    TP:
    - Support 130/300HZ report rate switching.
    
    Change-Id: I2a28e3a1486542f7bdc0066242c481e6a1d59658
    Signed-off-by: Jun Weng <wengjun1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3664589
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/focaltech_touch_v3_u_mmi/focaltech_ts_config.c
+++ drivers/input/touchscreen/focaltech_touch_v3_u_mmi/focaltech_ts_config.c
@@ -29,7 +29,8 @@ struct focaltech_ic_report_rate_config report_rate_config_info = {
 			.command = 1,
 		},
 	}
-#elif defined(PRODUCT_SCOUT) || defined(PRODUCT_CYBERT) || defined(PRODUCT_FUJI) || defined(PRODUCT_NAPLES)
+#elif defined(PRODUCT_SCOUT) || defined(PRODUCT_CYBERT) || defined(PRODUCT_FUJI) \
+	|| defined(PRODUCT_NAPLES) || defined(PRODUCT_MASERA)
 	.rate_config_count = 2,
 	.refresh_rate_ctrl = 0,
 	.interpolation_ctrl = 1,
