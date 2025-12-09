f541d2435f6b1d66848a9485fee16931823fb49e
Wang Yeqing <wangyq13@lenovo.com>
2025-12-09 15:25:49 +0800

    goodix_thp: clear the pen info when stylus leave the phone
    goodix_thp:
    clear the pen info when stylus leave the phone,
    because factory uses lots of pens to test, system may get old pen info
    if not clear the pen info value
    
    Change-Id: I14ddf1027d0f7f5684f28c34bc789e51c4f1487e
    Signed-off-by: Wang Yeqing <wangyq13@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3673656
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wei Deng <dengwei1@motorola.com>
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/goodix_thp/goodix_thp.c
+++ drivers/input/touchscreen/goodix_thp/goodix_thp.c
@@ -1632,9 +1632,11 @@ static long goodix_thp_input_agent_ioctl_set_coordinate(struct goodix_thp_core *
                     core_data->pen_state = PEN_STATE_NONE;
                     core_data->pen_close = 0;
                     core_data->uevent_message_type = PEN_MESSAGE_PEN_CLOSE;
+                    memset(core_data->pen_info, 0, sizeof(core_data->pen_info));
                     kobject_uevent(&core_data->pdev->dev.kobj, KOBJ_CHANGE);
                 } else if (core_data->pen_state == PEN_STATE_TOUCH) {
                     ts_info(tdev->dev, "touch_health - pen_action=UP");
+                    memset(core_data->pen_info, 0, sizeof(core_data->pen_info));
                     core_data->pen_state = PEN_STATE_NONE;
                 }
                 // --- release pen action detection end ---
