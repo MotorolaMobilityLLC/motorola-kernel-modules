46c0e1ff5171b70736b595bdc819de989eaa765b
Wenge Zhu <zhuwg1@motorola.com>
2025-12-29 14:49:42 +0800

    Disable touch input on inactive screen for foldables
    1. Problem
    On foldable devices, the touch controller for the inactive screen remains
    in gesture mode after a fold state change. For example, when the device is
    unfolded, the external CLI screen's touch controller is still active. This
    can lead to unintended touch events, prevent the system from sleeping,
    and cause unnecessary power consumption. A similar issue occurs for the main
    screen when the device is folded.
    
    2. Solution
    This change leverages the existing framework behavior of disabling all gestures
    on the inactive screen's touch controller during a fold state change.  When the
    touch driver receives the command to disable all gestures (via the 'gesture' sysfs node),
    it now checks if it was previously in gesture mode. If so, it calls a new
    `force_enter_standby_mode()` function. This transitions the touch controller to a low-power
    standby state, preventing it from generating interrupts and saving power.  This change is
    implemented behind a new build flag, `CONFIG_TOUCHCLASS_MMI_FORCE_ENTER_STANDBY`,
    to ensure it only affects targeted products.
    
    3. The timing when FWK sends the command to close all gestures
    1) Users manually turn off all gestures in the settings interface
    2) If the user has turned off all gestures by default, before pressing the power key to turn off the screen,
    FWK will send a command to turn off all gestures
    3) When the screen goes out due to the folded state, FWK will send a command to turn off all
    gestures before the screen goes out
    The screen is on before folding - the touch goes into sleep mode and then loses power
    Before folding, it is in a screen-off state - touch switches from sleep mode to standby mode
    
    4. Pay attention to the problem
    When the folding screen and the large screen light up, FWK will send a command to touch to exit stow mode
    when canceling the stow sensor. At this time, a judgment needs to be made to force it into standby mode,
    and forcing it into standby mode should be treated as a high priority.
    
    Change-Id: I483fe038e4c57f15f6cad47aa445abbb700f5ff2
    Signed-off-by: Wenge Zhu <zhuwg1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3700769
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Qiang Nie <nieqiang@motorola.com>
    Submit-Approved: Qiang Nie <nieqiang@motorola.com>

--- drivers/input/touchscreen/touchscreen_mmi/Android.mk
+++ drivers/input/touchscreen/touchscreen_mmi/Android.mk
@@ -37,7 +37,7 @@ ifeq ($(BOARD_USES_DOUBLE_TAP_CTRL),true)
 	KBUILD_OPTIONS += CONFIG_BOARD_USES_DOUBLE_TAP_CTRL=y
 endif
 
-ifeq ($(call is-board-platform-in-list, pineapple sun), true)
+ifeq ($(call is-board-platform-in-list, pineapple sun volcano), true)
 	KBUILD_OPTIONS += CONFIG_TOUCHSCREEN_DEVICE_VIRTUAL_PATH=y
 endif
 
@@ -51,6 +51,10 @@ endif
 
 ifeq ($(TOUCHCLASS_MMI_IRQ_ON_ONCE),true)
 	KBUILD_OPTIONS += CONFIG_TOUCHCLASS_MMI_IRQ_ON_ONCE=y
+endif
+
+ifeq ($(TOUCHCLASS_MMI_FORCE_ENTER_STANDBY),true)
+	KBUILD_OPTIONS += CONFIG_TOUCHCLASS_MMI_FORCE_ENTER_STANDBY=y
 endif
 
 include $(CLEAR_VARS)
--- drivers/input/touchscreen/touchscreen_mmi/touchscreen_mmi_class.c
+++ drivers/input/touchscreen/touchscreen_mmi/touchscreen_mmi_class.c
@@ -341,6 +341,9 @@ static ssize_t gesture_show(struct device *dev,
 static ssize_t gesture_store(struct device *dev,
 			struct device_attribute *attr, const char *buf, size_t size)
 {
+#ifdef CONFIG_TOUCHCLASS_MMI_FORCE_ENTER_STANDBY
+	int ret = 0;
+#endif
 	struct ts_mmi_dev *touch_cdev = dev_get_drvdata(dev);
 	unsigned int value = 0;
 	int err = 0;
@@ -383,6 +386,17 @@ static ssize_t gesture_store(struct device *dev,
 	}
 	mutex_unlock(&touch_cdev->extif_mutex);
 	dev_info(dev, "%s: gesture_mode_type = 0x%02x \n", __func__, touch_cdev->gesture_mode_type);
+
+#ifdef CONFIG_TOUCHCLASS_MMI_FORCE_ENTER_STANDBY
+	/* hal call this function twice to set the single/double tap, so we handle the gesture after double tap event */
+	if (value > 0x21) {
+		if (touch_cdev->gesture_mode_type == 0x0) {
+			TRY_TO_CALL(force_enter_standby_mode);
+		} else {
+			TRY_TO_CALL(exit_standby_mode);
+		}
+	}
+#endif
 
 	return size;
 }
