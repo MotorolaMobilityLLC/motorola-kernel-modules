6487a0c2ba46cfa2f77b8f5c23f2c6ef88f63cde
William Liu <a17671@motorola.com>
2026-01-04 13:56:50 +0800

    moto_swap5:Fix user space tombstones issue
    If the writebacked page not read after try read it from storage
    Retry the operation
    Remove unused warn print
    
    Change-Id: I7aa9441ddf0ceee9619687989f3b4f576783e2b6
    Signed-off-by: William Liu <a17671@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3705633
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wang Wang <wangwang@motorola.com>
    Submit-Approved: Jira Key

--- drivers/moto_swap5/zram-6.12/zram_drv.c
+++ drivers/moto_swap5/zram-6.12/zram_drv.c
@@ -1450,6 +1450,10 @@ static int zram_read_page(struct zram *zram, struct page *page, u32 index,
 {
 	int ret;
 
+#ifdef CONFIG_ZRAM_EXT
+	unsigned long timeout= jiffies + usecs_to_jiffies(100);
+retry:
+#endif
 	zram_slot_lock(zram, index);
 
 #ifdef CONFIG_HYBRIDSWAP_CORE
@@ -1474,10 +1478,10 @@ static int zram_read_page(struct zram *zram, struct page *page, u32 index,
 		 * device.
 		 */
 		zram_slot_unlock(zram, index);
-		ZRAM_CTX("fallback read_from_bdev, index =%lu", (unsigned long)index);
-		ZRAM_WARN_IF_ATOMIC_WAIT();
 #ifdef CONFIG_ZRAM_EXT
-		ret = try_read_from_bdev(zram, page, index, parent, false);
+		ret = try_read_from_bdev(zram, page, index, parent, time_before(jiffies,timeout));
+		if (ret== -ENOENT || ret == -EBUSY)
+			goto retry;
 #else
 		ret = read_from_bdev(zram, page, zram_get_element(zram, index),
 				     parent);
