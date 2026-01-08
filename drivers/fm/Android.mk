c90808cfabdad7264a06e083efbfd3e73d26167a
Hank Han <hanwq2@motorola.com>
2026-01-08 11:57:32 +0800

    [Bogota] add fm ctrl ko
    1. DDR rework
    2. add the node of elna fm for aidl service
    
    Change-Id: I16ea69c223725740870a1e1aa255a41d7dd35370
    Signed-off-by: Hank Han <hanwq2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3712527
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Tested-by: Jira Key
    Reviewed-by: Xinmin Fu <fuxm1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/fm/Android.mk
+++ drivers/fm/Android.mk
@@ -6,5 +6,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := fm_ctrl.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
 include $(DLKM_DIR)/AndroidKernelModule.mk
 endif
