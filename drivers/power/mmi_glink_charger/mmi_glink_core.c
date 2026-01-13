1eeba4121646b856df5c721921a2227829e3f812
lihaotian <liht19@motorola.com>
2026-01-13 20:09:43 +0800

    Horizon:Use the new printed BM_ULOG flag
    Modify the usage of 'chrg_present' to be a flag
    indicating whether to print BM_ULOG.
    Now use usb_online and wls_online instead.
    
    Change-Id: I865b474054017c1ee28d2ffe23553241648b41cf
    Signed-off-by: lihaotian <liht19@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3719401
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Wei Xu <xuwei9@lenovo.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/mmi_glink_charger/mmi_glink_core.c
+++ drivers/power/mmi_glink_charger/mmi_glink_core.c
@@ -728,13 +728,14 @@ static void mmi_get_charger_info(struct mmi_glink_chip *chip)
 		qti_encrypt_authentication(chip);
 	}
 
-	if(chip->charger_present_dynamic_control_bm_ulog && !bm_ulog_is_enabled_by_cmd()) {
-		if(chip->charger_info.chrg_present){
+	if (chip->charger_present_dynamic_control_bm_ulog && !bm_ulog_is_enabled_by_cmd()) {
+		if ((chip->charger_info.usb_online || chip->charger_info.wls_online) &&
+		    !chip->bm_ulog_enabled) {
 			// bm_ulog_enable_log(true, 1000);
 			bm_ulog_set_dynamic_debug_enabled(true);
 			chip->bm_ulog_enabled = true;
 			//mmi_info(chip, "enable adsp log during chg present!\n");
-		}else if(!chip->charger_info.chrg_present && chip->bm_ulog_enabled) {
+		}else if(!chip->charger_info.usb_online && !chip->charger_info.wls_online && chip->bm_ulog_enabled) {
 			// bm_ulog_enable_log(false, 0);
 			bm_ulog_set_dynamic_debug_enabled(false);
 			chip->bm_ulog_enabled = false;
