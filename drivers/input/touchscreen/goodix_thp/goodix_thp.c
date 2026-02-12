8f125ac88344512ff233a29dfb66a4b32114446b
dengwei <dengwei1@motorola.com>
2026-02-12 08:57:51 +0800

    blanc: input touch init stylus clk when pen connected
    we will init stylus clk when pen connected
    instead of bootloader.
    
    Change-Id: I9f8eaa85c584ce2e46bee8f13ed4cf67c9755c58
    Signed-off-by: dengwei <dengwei1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3766905
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/goodix_thp/goodix_thp.c
+++ drivers/input/touchscreen/goodix_thp/goodix_thp.c
@@ -2915,8 +2915,8 @@ static int goodix_ts_stylus_clk_init(struct goodix_thp_core *core_data)
         }
         ts_info(ts_dev->dev, "success get stylus suspend pinctrl state");
 
-        pinctrl_select_state(core_data->pinctrl, core_data->stylus_clk_suspend);
-        ts_info(ts_dev->dev, "set stylus suspend pinctrl state");
+   //     pinctrl_select_state(core_data->pinctrl, core_data->stylus_clk_suspend);
+   //     ts_info(ts_dev->dev, "set stylus suspend pinctrl state");
 
         return 0;
 exit_pinctrl_put:
