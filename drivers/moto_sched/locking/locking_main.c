a54f4d5aa1d9134e2dc5b5011332ed6ced9c7973
haiqin xu <xuhq6@motorola.com>
2026-03-09 11:37:09 +0800

    Revert "(CR):Revert "(CR): Alloc moto_task_struct and rework rwsem boost""
    This reverts commit 65a7d237f53e479709c3fad919ad0c224f279e0b.
    
    Change-Id: Ie809f1f410f022cedd0a8c6ae2bf2f8a7fa96788
    Reviewed-on: https://gerrit.mot.com/3797474
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Ravindranath Sattaru <sravindr@motorola.com>
    Submit-Approved: haiqin xu <xuhq6@motorola.com>
    Tested-by: haiqin xu <xuhq6@motorola.com>

--- drivers/moto_sched/locking/locking_main.c
+++ drivers/moto_sched/locking/locking_main.c
@@ -10,6 +10,8 @@
 
 #include "locking_main.h"
 
+bool locking_enabled = false;
+
 int locking_opt_init(void)
 {
 	int ret = 0;
@@ -25,6 +27,8 @@ int locking_opt_init(void)
 #ifdef CONFIG_MOTO_FUTEX_INHERIT
 	register_futex_vendor_hooks();
 #endif
+
+	locking_enabled = true;
 
 	return ret;
 }
