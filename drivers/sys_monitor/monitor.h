dcbb7437a0b2979a905281c4c8542107dfa4000e
Dingwei Luo <luodw1@motorola.com>
2026-03-09 16:21:10 +0800

    sys_monitor: add kernel wakelock  collecting
    add kernel wakelock  collecting
    Output the defference between the two readings.
    
    Change-Id: Id3ce4b9c877b913ee38b7fd8dc4ec61fd46cbd6f
    Signed-off-by: Dingwei Luo <luodw1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3798181
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/sys_monitor/monitor.h
+++ drivers/sys_monitor/monitor.h
@@ -43,4 +43,6 @@ void monitor_sleep_exit(void);
 int monitor_misc_init(struct kobject *parent_kobj);
 void monitor_misc_exit(struct kobject *parent_kobj);
 
+int wakelock_profile_init(struct kobject *parent_kobj);
+void wakelock_profile_exit(struct kobject *parent_kobj);
 #endif //__MONITOR_H__
