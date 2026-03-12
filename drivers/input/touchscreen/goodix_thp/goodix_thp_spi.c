8bd4b5c15337a1647d1e231c3df93f5f321e261a
Wenge Zhu <zhuwg1@motorola.com>
2026-03-12 13:42:46 +0800

    Optimize spi transfer stability for touch driver
    The spi master driver, spi-msm-geni.c, has a defect in the device's
    sleep-resume management.
    If the sleep function of the spi master does not call the sleep
    function for notifying the spi controller, then when the device
    uses spi_sync() for reading and writing, the operation of accessing
    the spi master registers will still proceed, resulting in problems
    accessing the resource of the spi master and causing the kernel to crash.
    
    spi_geni_suspend()
        spi_controller_suspend(spi)
    	ctlr->flags |= SPI_CONTROLLER_SUSPENDED
        spi_geni_runtime_suspend()
    
    __spi_sync()
        if (__spi_check_suspended(ctlr))
    	return -ESHUTDOWN;
    
    In order to enhance the stability of SPI transmission, we need to ensure
    that in our device driver, when the SPI master goes into sleep mode, it
    should no longer initiate SPI read or write operations.
    
    Change-Id: Id6c7ffd835dd09242d3a115e52e53e1e5376ad6a
    Signed-off-by: Wenge Zhu <zhuwg1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3804813
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/goodix_thp/goodix_thp_spi.c
+++ drivers/input/touchscreen/goodix_thp/goodix_thp_spi.c
@@ -568,6 +568,13 @@ static int goodix_thp_spi_read(struct thp_ts_device *dev, unsigned int addr,
         struct spi_transfer xfers;
         struct spi_message spi_msg;
         int ret = 0;
+        struct platform_device *pdev = spi_get_drvdata(spi);
+        struct goodix_thp_core *core_data = platform_get_drvdata(pdev);
+
+        if (unlikely(core_data->pm_suspend)) {
+            ts_err(&spi->dev, "spi master suspend");
+            return -EINVAL;
+        }
 
         mutex_lock(&dev->spi_mutex);
         spi_message_init(&spi_msg);
@@ -641,7 +648,7 @@ static int goodix_thp_spi_write(struct thp_ts_device *dev, unsigned int addr,
         struct platform_device *pdev = spi_get_drvdata(spi);
         struct goodix_thp_core *core_data = platform_get_drvdata(pdev);
 
-        if (core_data->pm_suspend) {
+        if (unlikely(core_data->pm_suspend)) {
             ts_err(&spi->dev, "system spi suspend");
             return -EINVAL;
         }
