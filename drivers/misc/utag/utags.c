5e5c1b60dd49d34d39339214aa2e3a3e5f9becc0
qianlong <qianl2@motorola.com>
2026-04-01 15:43:39 +0800

    utag: fix build error for kernel6.18
    error:
    /home/qianlong2/src/8975/moto_mainline_init_0323/kernel_platform/out/bazel/output_user_root/60fbafb12df6a9ad415dee434bb4eb42/sandbox/linux-sandbox/219/execroot/_main/motorola/kernel/modules/drivers/misc/utag/utags.c:2265:18: error: expected ';' after top level declarator
     2265 | MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
    
    Starting from Linux kernel 6.13, the MODULE_IMPORT_NS macro requires its argument
    to be a string literal (enclosed in double quotes). The kernel mainline removed the
    internal stringification from the macro definition to simplify the underlying symbol
    namespace implementation and improve parsing by module tools l(CR) modpost.
    
    Change-Id: Ie80f5efe1a57c25a57f14ead17d94dc49e8afdba
    Signed-off-by: qianlong <qianl2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3839757
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/utag/utags.c
+++ drivers/misc/utag/utags.c
@@ -2262,7 +2262,11 @@ module_exit(utags_exit);
 
 MODULE_LICENSE("GPL");
 #if KERNEL_VERSION(5, 4, 0) <= LINUX_VERSION_CODE
+#if KERNEL_VERSION(6, 13, 0) <= LINUX_VERSION_CODE
+MODULE_IMPORT_NS("VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver");
+#else
 MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+#endif
 #endif
 MODULE_AUTHOR("Motorola Mobility LLC");
 MODULE_DESCRIPTION("Configuration module");
